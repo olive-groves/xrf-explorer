f13da58963e012c2d291c484bb5ca30f2f0f6968
IMaxiM03I <maxilea202@gmail.com>
2024-05-02 17:05:34 +0200

    fixed circular import

--- xrf_explorer/__init__.py
+++ xrf_explorer/__init__.py
@@ -2,11 +2,12 @@ from pathlib import Path
 
 from flask import Flask, send_from_directory
 
-from xrf_explorer.server import routes
-
 app = Flask(__name__, template_folder=Path('client/templates'), static_folder='client/dist')
 app.config['SECRET_KEY'] = 'insert-more-secure-key'
 app.config['UPLOAD_FOLDER'] = 'server/files'
+
+
+from xrf_explorer.server import routes
 
 
 # All routes not matched in the server are forwarded to the client
