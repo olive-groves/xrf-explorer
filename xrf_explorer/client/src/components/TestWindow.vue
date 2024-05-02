4e8b3c0bd1cc2fed14474fe1f1cc09c2eb1004a8
Jan Bulthuis <jbulthuis03@gmail.com>
2024-05-02 20:15:44 +0200

    Removed test window

--- xrf_explorer/client/src/components/TestWindow.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { Window } from "@/components/ui/window";
-</script>
-
-<template>
-  <Window title="Test window" id="test_window">
-    Hello there.
-  </Window>
-</template>
--- xrf_explorer/client/src/components/index.ts
+++ xrf_explorer/client/src/components/index.ts
@@ -1,2 +1,1 @@
 export { default as Header } from './Header.vue';
-export { default as TestWindow } from './TestWindow.vue';
