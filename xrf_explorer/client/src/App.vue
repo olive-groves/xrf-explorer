fbca1fa544fa2230a58f483439af01b0230ca324
IMaxiM03I <maxilea202@gmail.com>
2024-05-02 16:33:15 +0200

    frontend implementation to send local file

--- xrf_explorer/client/src/App.vue
+++ xrf_explorer/client/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import UploadFile from "@/components/UploadFile.vue";
 </script>
 
 <template>
-  <HelloWorld />
+<!--  <HelloWorld />-->
+  <UploadFile />
 </template>
 
 <style scoped>
---
+++ xrf_explorer/client/src/components/UploadFile.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+import {Button} from "@/components/ui/button";
+</script>
+
+<template>
+  <head>
+    <Title>Upload a File here!</Title>
+  </head>
+  <body>
+    <form method="POST" enctype="multipart/form-data">
+      {{ form.hidden_tag() }}
+      {{ form.file() }}
+      {{ form.submit() }}
+    </form>
+    <button type="button" class="btn btn-success">Upload</button>
+  </body>
+</template>
+
+<style scoped>
+
+</style>
