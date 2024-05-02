2ef15f47bfeeabba4b1537587e54b6c52bf96491
Jan Bulthuis <jbulthuis03@gmail.com>
2024-05-02 21:10:20 +0200

    Updated App.vue and added a new TestWindow

--- xrf_explorer/client/src/App.vue
+++ xrf_explorer/client/src/App.vue
@@ -1,39 +1,17 @@
 <script setup lang="ts">
-import { Header, TestWindow } from "@/components";
-import { Window, WindowContainer } from "@/components/ui/window";
-import { Separator } from "@/components/ui/separator";
-import { Button } from "@/components/ui/button";
+import { Header } from "@/components";
+import { TestWindow } from "@/windows";
+import { DraggableContainer } from "@/components/functional/draggable";
 </script>
 
 <template>
-  <div class="grid grid-cols-1 grid-rows-[min-content_1fr] w-[100vw] h-[100vh]">
+  <div class="w-[100vw] h-[100vh]">
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Farmhouse_in_Provence%2C_1888%2C_Vincent_van_Gogh%2C_NGA.jpg/2048px-Farmhouse_in_Provence%2C_1888%2C_Vincent_van_Gogh%2C_NGA.jpg"
+      class="w-full h-full -z-20 fixed left-0 top-0" />
     <Header />
-    <WindowContainer class="w-full h-full border-t">
-      <Window title="Example Window" id="example">
-        <div class="space-y-2">
-          <div>
-            A cool window
-          </div>
-          <Separator />
-          <div>
-            This is a separator, it separates things.
-          </div>
-          <img class="w-48 mx-auto" src="https://iiif.micr.io/vYtZI/full/600,/0/default.webp" />
-          <div>
-            You can basically put everything you want in these windows.
-          </div>
-          <Button>
-            Cool button :0
-          </Button>
-        </div>
-      </Window>
-      <Window title="Second wider window" id="example_2" opened :width="30">
-        Here is some more content for in the window, but what happens if the text gets too long and needs to be
-        wrapped
-        to seperate lines?
-      </Window>
+    <DraggableContainer class="w-full h-full fixed left-0 top-0">
       <TestWindow />
-    </WindowContainer>
+    </DraggableContainer>
   </div>
-  <!-- <canvas class="h-full w-full" id="glcanvas"/> -->
 </template>
--- xrf_explorer/client/src/components/Header.vue
+++ xrf_explorer/client/src/components/Header.vue
@@ -17,101 +17,64 @@ import {
   //   MenubarSubTrigger,
   MenubarTrigger,
 } from '@/components/ui/menubar'
-import {
-  MenubarWindowItem
-} from '@/components/ui/window'
+import { WindowMenu } from '@/components/ui/window'
 </script>
 
 <template>
-  <div class="flex flex-row space-x-2 px-4">
+  <Menubar class="w-min m-2 z-50 fixed">
     <div>
-      <div class="h-full content-center font-bold select-none">
+      <div class="h-full content-center font-bold text-nowrap px-3">
         XRF-Explorer
       </div>
     </div>
-    <Menubar class="rounded-none border-none">
-      <MenubarMenu>
-        <MenubarTrigger>
-          Workspace
-        </MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem>
-            Recent #1
-          </MenubarItem>
-          <MenubarItem>
-            Recent #2
-          </MenubarItem>
-          <MenubarItem>
-            Recent #3
-          </MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>
-            Open workspace
-          </MenubarItem>
-          <MenubarSub>
-            <MenubarSubTrigger>
-              Open recent
-            </MenubarSubTrigger>
-            <MenubarSubContent>
-              <MenubarItem>
-                Recent #1
-              </MenubarItem>
-              <MenubarItem>
-                Recent #2
-              </MenubarItem>
-              <MenubarItem>
-                Recent #3
-              </MenubarItem>
-              <MenubarItem>
-                Recent #4
-              </MenubarItem>
-              <MenubarItem>
-                Recent #5
-              </MenubarItem>
-              <MenubarItem>
-                Recent #6
-              </MenubarItem>
-            </MenubarSubContent>
-          </MenubarSub>
-          <MenubarItem>
-            New workspace
-          </MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger>
-          View
-        </MenubarTrigger>
-        <MenubarContent>
-          <MenubarWindowItem window_id="example">
-            Example #1
-          </MenubarWindowItem>
-          <MenubarWindowItem window_id="example_2">
-            Example #2
-          </MenubarWindowItem>
-          <MenubarSeparator />
-          <MenubarWindowItem window_id="test_window">
-            Test window
-          </MenubarWindowItem>
-        </MenubarContent>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger>
-          Help
-        </MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem>
-            Documentation
-          </MenubarItem>
-          <MenubarItem>
-            Github
-          </MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem>
-            About
-          </MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-    </Menubar>
-  </div>
+    <MenubarMenu>
+      <MenubarTrigger>
+        Workspace
+      </MenubarTrigger>
+      <MenubarContent>
+        <MenubarItem>
+          Recent #1
+        </MenubarItem>
+        <MenubarItem>
+          Recent #2
+        </MenubarItem>
+        <MenubarItem>
+          Recent #3
+        </MenubarItem>
+        <MenubarSeparator />
+        <MenubarItem>
+          Open workspace
+        </MenubarItem>
+        <MenubarSub>
+          <MenubarSubTrigger>
+            Open recent
+          </MenubarSubTrigger>
+          <MenubarSubContent>
+            <MenubarItem>
+              Recent #1
+            </MenubarItem>
+            <MenubarItem>
+              Recent #2
+            </MenubarItem>
+            <MenubarItem>
+              Recent #3
+            </MenubarItem>
+            <MenubarItem>
+              Recent #4
+            </MenubarItem>
+            <MenubarItem>
+              Recent #5
+            </MenubarItem>
+            <MenubarItem>
+              Recent #6
+            </MenubarItem>
+          </MenubarSubContent>
+        </MenubarSub>
+        <MenubarItem>
+          New workspace
+        </MenubarItem>
+      </MenubarContent>
+    </MenubarMenu>
+    <WindowMenu />
+  </Menubar>
 </template>
