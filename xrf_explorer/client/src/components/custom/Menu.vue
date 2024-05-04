2a8984ea9e7e306e2ecdb3623e99692444317872
Ivan Ivanov <ivanivanov.jsg@gmail.com>
2024-05-04 22:24:11 +0200

    Menu finished

--- xrf_explorer/client/src/components/custom/Menu.vue
+++ xrf_explorer/client/src/components/custom/Menu.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 import Button from "@/components/ui/button/Button.vue";
+
+const buttonProps = {
+  variant: "outline",
+  size: "lg",
+  class: "font-bold text-lg",
+  style: "background-color: #231f20",
+};
 </script>
 
 <template>
-  <div style="background-color: #2b2828" class="pl-1 pt-1 pb-1">
-    <Button variant="outline" size="lg" class="font-bold text-lg">
-      Files
-    </Button>
-    <Button variant="outline" size="lg" class="font-bold text-lg ml-1">
-      View
-    </Button>
+  <div style="background-color: #2b2828" class="grid gap-4 grid-cols-12 p-1">
+    <Button v-bind="buttonProps"> Files </Button>
+    <Button v-bind="buttonProps"> View </Button>
+    <div
+      style="display: inline"
+      class="col-start-5 col-end-9 mx-auto text-3xl font-bold"
+    >
+      XRF Explorer 2.0
+    </div>
+    <Button v-bind="buttonProps" class="col-start-12">Reset</Button>
   </div>
 </template>
 
