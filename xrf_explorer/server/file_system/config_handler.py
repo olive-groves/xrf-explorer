63148396b24f303951a63f18ea05e9cd27201837
IMaxiM03I <maxilea202@gmail.com>
2024-05-07 12:06:30 +0200

    catch exception where config path does not exist

--- xrf_explorer/server/file_system/config_handler.py
+++ xrf_explorer/server/file_system/config_handler.py
@@ -6,9 +6,9 @@ LOG: logging.Logger = logging.getLogger(__name__)
 
 
 def load_yml(path: str) -> dict:
-    with open(path, 'r') as config_file:
-        try:
+    try:
+        with open(path, 'r') as config_file:
             return yaml.safe_load(config_file)
-        except yaml.YAMLError:
-            LOG.exception("Failed to access config at {%s}", path)
-            return {}
+    except (FileNotFoundError, yaml.YAMLError):
+        LOG.exception("Failed to access config at {%s}", path)
+        return {}
