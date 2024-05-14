883c9e3f3675e720ddf9f2851fb56f0cbe0d6d30
Dirk <d.burgers@student.tue.nl>
2024-05-14 11:42:33 +0200

    Fixed test

--- tests/file_system/test_file_upload.py
+++ tests/file_system/test_file_upload.py
@@ -45,34 +45,11 @@ class TestUploadFileToServer:
         assert expected_output in caplog.text
     
     def test_get_file_names(self):
-        # constants
+        # setup
         filename: str = "test_file_name.txt"
 
-        # open file
-        f = open(self.UPLOAD_FILE_PATH, "rb")
-        
-        # setup
-        file: FileStorage = FileStorage(f)
-        file.filename = FILENAME
-
-        # upload file
-        result: bool = upload_file_to_server(file, self.CUSTOM_CONFIG_PATH)
-        
-        # close file
-        file.close()
-
-        # check if upload file was succussful
-        assert bool
-
-        # get files from server
+        # execute
         result: list[str] = get_files(self.CUSTOM_CONFIG_PATH)
         
-        # Check if file was uploaded
-        assert FILENAME in result
-
-        # remove test file 
-        config: dict = load_yml(self.CUSTOM_CONFIG_PATH)
-        
-        assert config
-
-        remove(Path(config['uploads-folder'], FILENAME))
+        # validate
+        assert filename in result
