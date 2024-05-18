041ab90dee3bb4277c2c4ab5ab99092591e14f4e
Ivan Ivanov <ivanivanov.jsg@gmail.com>
2024-05-18 18:21:02 +0200

    Implemented client side logic for chunked uploading

--- xrf_explorer/client/src/components/dialogs/UploadFileDialog.vue
+++ xrf_explorer/client/src/components/dialogs/UploadFileDialog.vue
@@ -8,14 +8,6 @@ import {
 } from "@/components/ui/dialog";
 import { ref, Ref } from "vue";
 
-const dataSourceNameInput = ref<HTMLInputElement>()!;
-const rgbImageInput = ref<HTMLInputElement>();
-const uvImageInput = ref<HTMLInputElement>();
-const xrayImageInput = ref<HTMLInputElement>();
-const cubeDataInput = ref<HTMLInputElement>();
-const rawDataInput = ref<HTMLInputElement>();
-const rplDataInput = ref<HTMLInputElement>();
-
 /**
  * Retrieves the first file from a ref object pointing to an input element.
  * @returns {File | undefined} The first file selected in the input, or undefined if no files are present.
@@ -26,61 +18,85 @@ function getFile(
   return inputRef.value?.files![0];
 }
 
+const dataSourceNameInputRef = ref<HTMLInputElement>()!;
+const rgbImageInputRef = ref<HTMLInputElement>();
+const uvImageInputRef = ref<HTMLInputElement>();
+const xrayImageInputRef = ref<HTMLInputElement>();
+const cubeDataInputRef = ref<HTMLInputElement>();
+const rawDataInputRef = ref<HTMLInputElement>();
+const rplDataInputRef = ref<HTMLInputElement>();
+
 /**
  * Handles the uploading of data source files to the server.
  * Triggered by the "Upload" button.
  */
-function uploadDataSource() {
-  const formData = new FormData();
+async function uploadDataSource() {
+  const dataSourceName: string = dataSourceNameInputRef.value?.value.trim()!;
 
-  const dataSourceName: string = dataSourceNameInput.value?.value.trim()!;
-  const rgbImageFile = getFile(rgbImageInput);
-  const uvImageFile = getFile(uvImageInput);
-  const xrayImageFile = getFile(xrayImageInput);
-  const cubeDataFile = getFile(cubeDataInput);
-  const rawDataFile = getFile(rawDataInput);
-  const rplDataFile = getFile(rplDataInput);
+  const inputRefsWithFiles = [
+    rgbImageInputRef,
+    uvImageInputRef,
+    xrayImageInputRef,
+    cubeDataInputRef,
+    rawDataInputRef,
+    rplDataInputRef,
+  ].filter((input) => input.value?.files![0] !== undefined);
 
   if (dataSourceName === "") {
     alert("A non-empty data source name must be provided.");
     return;
   }
 
-  if (rgbImageFile === undefined) {
+  if (getFile(rgbImageInputRef) === undefined) {
     alert("RGB image must be provided.");
     return;
   }
 
   if (
-    cubeDataFile === undefined &&
-    (rawDataFile === undefined || rplDataInput === undefined)
+    getFile(cubeDataInputRef) === undefined &&
+    (getFile(rawDataInputRef) === undefined ||
+      getFile(rplDataInputRef) === undefined)
   ) {
     alert("Raw or processed data must be provided.");
     return;
   }
 
-  formData.append("name", dataSourceName);
-  formData.append("rgb", rgbImageFile);
+  const formDataDsName = new FormData();
+  formDataDsName.append("name", dataSourceName);
 
-  // NOTE: This conditional statement assumes that .raw and .rpl files must always be uploaded together. (URF 2.1)
-  if (rawDataFile !== undefined && rplDataFile !== undefined) {
-    formData.append("raw", rawDataFile);
-    formData.append("rpl", rplDataFile);
-  }
+  let response: Response = await fetch("/api/create-ds-dir", {
+    method: "POST",
+    body: formDataDsName,
+  });
+  let jsonResponse = await response.json();
+  const dataSourceDirName: string = jsonResponse["dataSourceDir"];
 
-  if (cubeDataFile !== undefined) {
-    formData.append("cube", cubeDataFile);
-  }
+  console.log("The name of the data source dir is " + dataSourceDirName);
+  console.log("The input refs which have files are " + inputRefsWithFiles);
 
-  if (uvImageFile !== undefined) {
-    formData.append("uv", uvImageFile);
-  }
+  inputRefsWithFiles.forEach((inputRef) => {
+    const file: File = getFile(inputRef)!;
+    const fileType: string = file.name.split(".").pop()!;
+    const uploadFileName = inputRef.value?.name + "." + fileType;
+    const chunkSize = 3000000 / 2; // in bytes, therefore 1 MB
 
-  if (xrayImageFile !== undefined) {
-    formData.append("xray", xrayImageFile);
-  }
+    for (let byteIndex = 0; byteIndex < file?.size!; byteIndex += chunkSize) {
+      const chunk: Blob = file!.slice(byteIndex, byteIndex + chunkSize);
 
-  fetch("/api/upload-data-source", { method: "POST", body: formData });
+      const formDataSendChunks = new FormData();
+      formDataSendChunks.append(
+        "dir",
+        dataSourceDirName + "/" + uploadFileName,
+      );
+      formDataSendChunks.append("startByte", String(byteIndex));
+      formDataSendChunks.append("chunkBytes", chunk);
+
+      fetch("/api/upload-file-chunk", {
+        method: "POST",
+        body: formDataSendChunks,
+      });
+    }
+  });
 }
 </script>
 
@@ -91,10 +107,10 @@ function uploadDataSource() {
     <div class="flex items-center">
       <label class="w-64">Data source name</label>
       <input
-        type="text"
         class="bg-accent text-accent-foreground rounded-lg focus:ring-ring focus:border-border block w-44 p-2"
         placeholder="Data source"
-        ref="dataSourceNameInput"
+        type="text"
+        ref="dataSourceNameInputRef"
       />
     </div>
     <div class="flex items-center">
@@ -103,7 +119,8 @@ function uploadDataSource() {
         class="block w-44 text-accent-foreground rounded-lg cursor-pointer bg-background focus:outline-none"
         aria-describedby="rgb_input_help"
         type="file"
-        ref="rgbImageInput"
+        name="rgb"
+        ref="rgbImageInputRef"
       />
     </div>
     <div class="flex items-center">
@@ -112,7 +129,8 @@ function uploadDataSource() {
         class="block w-44 text-accent-foreground rounded-lg cursor-pointer bg-background focus:outline-none"
         aria-describedby="uv_input_help"
         type="file"
-        ref="uvImageInput"
+        name="uv"
+        ref="uvImageInputRef"
       />
     </div>
     <div class="flex items-center">
@@ -121,7 +139,8 @@ function uploadDataSource() {
         class="block w-44 text-accent-foreground rounded-lg cursor-pointer bg-background focus:outline-none"
         aria-describedby="xray_input_help"
         type="file"
-        ref="xrayImageInput"
+        name="xray"
+        ref="xrayImageInputRef"
       />
     </div>
     <div class="flex items-center">
@@ -130,7 +149,8 @@ function uploadDataSource() {
         class="block w-44 text-accent-foreground rounded-lg cursor-pointer bg-background focus:outline-none"
         aria-describedby="cube_input_help"
         type="file"
-        ref="cubeDataInput"
+        name="cube"
+        ref="cubeDataInputRef"
       />
     </div>
     <div class="flex items-center">
@@ -139,7 +159,8 @@ function uploadDataSource() {
         class="block w-44 text-accent-foreground rounded-lg cursor-pointer bg-background focus:outline-none"
         aria-describedby="raw_data_input_help"
         type="file"
-        ref="rawDataInput"
+        name="raw"
+        ref="rawDataInputRef"
       />
     </div>
     <div class="flex items-center">
@@ -148,7 +169,8 @@ function uploadDataSource() {
         class="block w-44 text-accent-foreground rounded-lg cursor-pointer bg-background focus:outline-none"
         aria-describedby="raw_data_input_help"
         type="file"
-        ref="rplDataInput"
+        name="rpl"
+        ref="rplDataInputRef"
       />
     </div>
 
