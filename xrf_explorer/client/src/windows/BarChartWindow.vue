24e965c22ecb6e2eec594fe14b44b5ec3ea016d9
Iliyan Teofilov <iliyanvt18@gmail.com>
2024-05-20 14:59:06 +0200

    Fixed code style

--- xrf_explorer/client/src/windows/BarChartWindow.vue
+++ xrf_explorer/client/src/windows/BarChartWindow.vue
@@ -1,137 +1,170 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { Window } from '@/components/ui/window';
-import { AspectRatio } from '@/components/ui/aspect-ratio';
-import { watch } from 'vue';
-import * as d3 from 'd3';
+import { ref } from "vue";
+import { Window } from "@/components/ui/window";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { watch } from "vue";
+import * as d3 from "d3";
 
-const url = 'http://localhost:8001';
+const url = "http://localhost:8001";
 
 const barchart = ref(null);
 
 type Element = {
-    name: string;
-    average: number;
+  name: string;
+  average: number;
 };
 
 // Elemental data averages
-var dataAverages: Element[];
+let dataAverages: Element[];
 
+/**
+ * Fetch the average elemental data for each of the elements, and store it
+ * in the `dataAverages` array.
+ * 
+ * @param url URL to the server which provides the elemental data.
+ */
 async function fetchAverages(url: string) {
-    // Make API call
-    const response: Response = await fetch(`${url}/api/element_average`, {
-        method: "GET",
-        headers: {
-            "Content-Type":"application/json"
-        }
-    });
-    let fetchSuccessful: boolean = false;
+  // Make API call
+  const response: Response = await fetch(`${url}/api/element_average`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  let fetchSuccessful: boolean = false;
 
-    // Check that fetching the names was successful
-    if (response.ok) {
-        // Save the names
-        fetchSuccessful = await response.json().then(data => {
-            dataAverages = data;
-            console.log("Successfully fetched averages");
-            return true;
-        }).catch(e => {
-            console.log(e);
-            return false;
-        })
-    } else {
-        // Error response
-        fetchSuccessful = await response.text().then(data => {
-            console.log(data);
-            return false;
-        }).catch(e => {
-            console.log(e);
-            return false;
-        });
-    }
+  // Check that fetching the names was successful
+  if (response.ok) {
+    // Save the names
+    fetchSuccessful = await response
+      .json()
+      .then((data) => {
+        dataAverages = data;
+        console.log("Successfully fetched averages");
+        return true;
+      })
+      .catch((e) => {
+        console.log(e);
+        return false;
+      });
+  } else {
+    // Error response
+    fetchSuccessful = await response
+      .text()
+      .then((data) => {
+        console.log(data);
+        return false;
+      })
+      .catch((e) => {
+        console.log(e);
+        return false;
+      });
+  }
 
-    return fetchSuccessful;
+  return fetchSuccessful;
 }
 
+/**
+ * Set up the bar chart's SVG container, add axes and data.
+ */
 function setup() {
-    // Declare chart dimensions and margins
-    const margin = {top: 30, right: 30, bottom: 70, left: 60},
-        width = 860 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
-    const max: number = <number>d3.max(dataAverages, (d) => d.average);
+  // Declare chart dimensions and margins
+  const margin = { top: 30, right: 30, bottom: 70, left: 60 },
+    width = 860 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+  const max: number = d3.max(dataAverages, (d) => d.average) as number;
 
-    // Select SVG container
-    const svg = d3.select(barchart.value)
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [0, 0, width, height])
-        .attr("style", "max-width: 100%; height: auto;");
+  // Select SVG container
+  const svg = d3
+    .select(barchart.value)
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [0, 0, width, height])
+    .attr("style", "max-width: 100%; height: auto;");
 
-    // Declare the horizontal position scale
-    const x = d3.scaleBand()
-        .domain(dataAverages.map(d => d.name))
-        .range([margin.left, width - margin.right])
-        .padding(0.1);
+  // Declare the horizontal position scale
+  const x = d3
+    .scaleBand()
+    .domain(dataAverages.map((d) => d.name))
+    .range([margin.left, width - margin.right])
+    .padding(0.1);
 
-    // Declare the vertical position scale
-    const y = d3.scaleLinear()
-        .domain([0, max])
-        .range([height - margin.bottom, margin.top]);
+  // Declare the vertical position scale
+  const y = d3
+    .scaleLinear()
+    .domain([0, max])
+    .range([height - margin.bottom, margin.top]);
 
-    // Add axes
-    svg.append("g")
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(x).tickSizeOuter(0))
-        .selectAll("text")
-            .attr("transform", "translate(-13, 15)rotate(-45)");
-    svg.append("g")
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y))
-        .call(g => g.select(".domain").remove())
-        .call(g => g.selectAll(".tick line").clone()
-            .attr("x2", width - margin.left - margin.right)
-            .attr("stroke-opacity", 0.1))
-        .call(g => g.append("text")
-            .attr("x", -margin.left)
-            .attr("y", 10)
-            .attr("fill", "currentColor")
-            .attr("text-anchor", "start")
-            .text("↑ Average concentration"));
+  // Add axes
+  svg
+    .append("g")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).tickSizeOuter(0))
+    .selectAll("text")
+    .attr("transform", "translate(-13, 15)rotate(-45)");
+  svg
+    .append("g")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y))
+    .call((g) => g.select(".domain").remove())
+    .call((g) =>
+      g
+        .selectAll(".tick line")
+        .clone()
+        .attr("x2", width - margin.left - margin.right)
+        .attr("stroke-opacity", 0.1),
+    )
+    .call((g) =>
+      g
+        .append("text")
+        .attr("x", -margin.left)
+        .attr("y", 10)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "start")
+        .text("↑ Average concentration"),
+    );
 
-    // Add data
-    svg.selectAll("svg")
-      .data(dataAverages)
-      .join("rect")
-        .attr("x", d => <number>x(d.name))
-        .attr("y", d => y(d.average))
-        .attr("width", x.bandwidth())
-        .attr("height", d => y(0) - y(d.average))
-        .attr("fill", "steelblue");
+  // Add data
+  svg
+    .selectAll("svg")
+    .data(dataAverages)
+    .join("rect")
+    .attr("x", (d) => x(d.name) as number)
+    .attr("y", (d) => y(d.average))
+    .attr("width", x.bandwidth())
+    .attr("height", (d) => y(0) - y(d.average))
+    .attr("fill", "steelblue");
 }
 
+/**
+ * Show the bar chart. This function includes the fetching of the elemental data
+ * which is displayed in the chart.
+ * 
+ * @param url URL to the server which provides the elemental data.
+ */
 async function showChart(url: string) {
-    try {
-        // Whether the elemental data was fetched properly
-        const fetched: boolean = await fetchAverages(url);
-        if (fetched)
-            setup();    // If everything went right, display the chart
-    } catch (e) {
-        console.error("Error fetching average data", e)
-    }
+  try {
+    // Whether the elemental data was fetched properly
+    const fetched: boolean = await fetchAverages(url);
+    if (fetched) setup(); // If everything went right, display the chart
+  } catch (e) {
+    console.error("Error fetching average data", e);
+  }
 }
 
 // Display the bar chart once the window is loaded
 watch(barchart, (n, o) => {
-    console.log(n, o)
-    if (barchart != null) {
-        showChart(url);
-    }
+  console.log(n, o);
+  if (barchart.value != null) {
+    showChart(url);
+  }
 });
 </script>
 
 <template>
-    <Window title="Bar Chart Window" opened>
-        <AspectRatio :ratio="4/3">
-            <svg ref="barchart"></svg>
-        </AspectRatio>
-    </Window>
+  <Window title="Bar Chart Window" opened>
+    <AspectRatio :ratio="4 / 3">
+      <svg ref="barchart"></svg>
+    </AspectRatio>
+  </Window>
 </template>
