8e7336c8f7780c487c00f4073c8df1b295614500
Adrien Verriele <a.a.m.verriele@student.tue.nl>
2024-05-20 22:12:46 +0200

    fixed tests

--- tests/test_spectra.py
+++ tests/test_spectra.py
@@ -4,21 +4,23 @@ class TestSpectra:
     rpl_path = 'C:/Users/20210792/Downloads/info.rpl'
     raw_path = 'C:/Users/20210792/Downloads/spectrum.raw'
     
-    def test_get_average_global():
+    def test_get_average_global(self, caplog):
         data = np.ndarray([[[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
                            [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]])
         low = 1
         high = 3
         bin_size = 1
         result = get_average_global(data, low, high, bin_size)
-        assert result == [{"index": 1, "value": 2}, {"index": 2, "value": 3}]
+        expected_result = [{"index": 1, "value": 2}, {"index": 2, "value": 3}]
+        assert expected_result in caplog.text
         
-    def test_get_average_selection():
+    def test_get_average_selection(self, caplog):
         data = np.ndarray([[[3, 4, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]],
                            [[1, 2, 3, 4], [1, 2, 1, 2], [1, 2, 3, 4]]])
         low = 1
         high = 3
         bin_size = 1
         pixels = [[0,0][1,1]]
-        result = get_average_selection(data, low, high, bin_size)
-        assert result == [{"index": 1, "value": 3}, {"index": 2, "value": 2}]
+        result = get_average_selection(data, pixels, low, high, bin_size)
+        expected_result = [{"index": 1, "value": 3}, {"index": 2, "value": 2}]
+        assert expected_result in caplog.text
