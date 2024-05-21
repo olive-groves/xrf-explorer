cb7077c9ed62939c0054ef6845810854af19aa6b
IMaxiM03I <maxilea202@gmail.com>
2024-05-21 20:58:43 +0200

    fix typos and formatting

--- xrf_explorer/server/dim_reduction/embedding.py
+++ xrf_explorer/server/dim_reduction/embedding.py
@@ -1,7 +1,6 @@
-import logging 
+import logging
 
 from os.path import join, abspath
-from pathlib import Path
 
 import numpy as np
 from umap import UMAP
@@ -12,27 +11,27 @@ from xrf_explorer.server.dim_reduction.general import valid_element, get_element
 LOG: logging.Logger = logging.getLogger(__name__)
 
 
-def apply_umap(data: np.ndarray, n_neighbors: int, min_dist: float, n_components: int, metric: str) -> np.ndarray | None:
+def apply_umap(data: np.ndarray, n_neighbors: int, min_dist: float, n_components: int,
+               metric: str) -> np.ndarray | None:
     """Reduces the dimensionality of the given data using uniform manifold approximation and projection (UMAP).
     The original data is not modified. For more information on UMAP, see: https://umap-learn.readthedocs.io/en/latest/.
 
-    :param data: array, shape (n_samples, n_features). The data on which UMAP is used to reduce the dimension of n-features to n_components. 
+    :param data: np.ndarray, shape (n_samples, n_features). The data on which UMAP is used to reduce the dimension of -features to n_components.
     :param n_neighbors: The size of local neighborhood. See UMAP documentation for more information.
     :param min_dist: The minimum distance between points in the embedding. See UMAP documentation for more information.
     :param n_components: The dimension of the embedded space. See UMAP documentation for more information.
     :param metric: The metric to use for distance computation. See UMAP documentation for more information.
-    :return: array, shape (n_samples, n_components) containing the result of UMAP applied to given data with the given parameters. 
-    If UMAP fails, None is returned.
+    :return: np.ndarray, shape (n_samples, n_components) containing the result of UMAP applied to given data with the given parameters. If UMAP fails, None is returned.
     """
 
     try:
         embedding: np.ndarray = UMAP(
             n_neighbors=n_neighbors,
             min_dist=min_dist,
-            n_components=n_components, 
+            n_components=n_components,
             metric=metric
         ).fit_transform(data)
-    
+
         return embedding
     except:
         return None
@@ -53,8 +52,9 @@ def filter_elemental_cube(elemental_cube: np.ndarray, element: int, threshold: i
     return indices
 
 
-def generate_embedding(element: int, threshold: int, umap_parameters: dict[str, str] = {}, config_path: str = "config/backend.yml") -> bool:
-    """Generate the embedding (lower dimensional reprensentation of the data) of the 
+def generate_embedding(element: int, threshold: int, umap_parameters: dict[str, str] = {},
+                       config_path: str = "config/backend.yml") -> bool:
+    """Generate the embedding (lower dimensional representation of the data) of the
     elemental data cube using the dimensionality reduction method "UMAP". The embedding 
     with the list of indices (which pixels from the elemental data cube are in the embedding) 
     are stored in the folder specified in the backend config file. The order the indices
@@ -65,7 +65,7 @@ def generate_embedding(element: int, threshold: int, umap_parameters: dict[str, 
     :param threshold: The threshold to filter the data cube by.
     :param umap_parameters: The parameters passed on to the UMAP algorithm.
     :param config_path: Path to the backend config file
-    :return: True if the embedding was successfully generated. Otherwise False.
+    :return: True if the embedding was successfully generated. Otherwise, False.
     """
 
     # load backend config
@@ -73,7 +73,7 @@ def generate_embedding(element: int, threshold: int, umap_parameters: dict[str, 
     if not backend_config:  # config is empty
         LOG.error("Failed to compute DR embedding")
         return False
-    
+
     # get default dim reduction config
     default_umap_parameters: dict[str, str] = backend_config['dim-reduction']['umap-parameters']
 
@@ -90,7 +90,7 @@ def generate_embedding(element: int, threshold: int, umap_parameters: dict[str, 
     # check if element is valid
     if not valid_element(element, data_cube):
         return False
-    
+
     # filter data
     indices: np.ndarray = filter_elemental_cube(data_cube, element, threshold)
     filtered_data: np.ndarray = data_cube[indices[:, 0], indices[:, 1], :]
@@ -100,17 +100,17 @@ def generate_embedding(element: int, threshold: int, umap_parameters: dict[str, 
              f"\telement: {element},\n"
              f"\tthreshold: {threshold},\n"
              f"\tsize: {filtered_data.shape}")
-    
+
     embedded_data: np.ndarray | None = apply_umap(
         filtered_data,
-        int(modified_umap_parameters['n-neighbors']), 
-        float(modified_umap_parameters['min-dist']), 
-        int(modified_umap_parameters['n-components']), 
+        int(modified_umap_parameters['n-neighbors']),
+        float(modified_umap_parameters['min-dist']),
+        int(modified_umap_parameters['n-components']),
         modified_umap_parameters['metric']
     )
 
     if embedded_data is None:
-        LOG.error(f"Failed to compute embedding")
+        LOG.error("Failed to compute embedding")
         return False
 
     # save indices and embedded data
