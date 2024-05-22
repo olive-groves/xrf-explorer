d8f93e477f0e9db9d672cad67204afd4584ce3cc
SoniaMaxim <sony.maxim31@gmail.com>
2024-05-22 15:04:54 +0200

    cleaned up code and changed parameters

--- xrf_explorer/server/color_seg.py
+++ xrf_explorer/server/color_seg.py
@@ -1,8 +1,6 @@
 import numpy as np
-import time
 import matplotlib.pyplot as plt
 import cv2
-from sklearn.cluster import DBSCAN
 from PIL import Image
 from skimage import color
 import skimage
@@ -64,10 +62,6 @@ def merge_similar_colors(clusters, t):
             col2 = rgb_to_lab(clusters[j])
             # if the color difference between two clusters is lower than the defined threshold
             if (calculate_color_difference(col1, col2) < t):
-                # perform union of bitmasks
-                # bitmask[i] = bitmask[j] | bitmask[i]
-
-                # bitmask.pop(j)
                 # set the new color of the cluster as the average between the colors of the two merged clusters
                 clusters[i] = [np.sqrt((col1[0]**2+col2[0]**2)/2),np.sqrt((col1[1]**2+col2[1]**2)/2),np.sqrt((col1[2]**2+col2[2]**2)/2)]                # convert color if cluster back to rgb
                 clusters[i] = lab_to_rgb(clusters[i])
@@ -83,7 +77,7 @@ def merge_similar_colors(clusters, t):
 ########################################################################################################################
 
 
-def get_clusters_using_k_means(small_image, nr_of_attempts=20, k=20):
+def get_clusters_using_k_means(small_image, nr_of_attempts=10, k = 30):
     """Extract the color clusters of the resized RGB image using the k-means clustering method in OpenCV
 
     :param small_image: the resized image to apply the k-means on
@@ -108,73 +102,6 @@ def get_clusters_using_k_means(small_image, nr_of_attempts=20, k=20):
 
     return label, center
 
-
-########################################################################################################################
-# DBSCAN ###############################################################################################################
-########################################################################################################################
-
-
-def get_clusters_using_dbscan(small_image, eps=1.5, min_samples=20):
-    """
-    Returns a list of LAB color triples corresponding to the color clusters. Found using DBSCAN.
-    :param small_image: The scaled down version of the image.
-    :param eps: A value to indicate the maximum distance between two colors to still be considered "similar".
-    :param min_samples: The minimum number of neighbouring points required to be considered a core point.
-    :return: The list of LAB color triples corresponding to the color clusters.
-    """
-
-    lab_colors = image_to_lab(small_image)
-    data = np.array(lab_colors)
-
-    # Initialize DBSCAN
-    dbscan = DBSCAN(eps=eps, min_samples=min_samples)
-    # Fit the model and predict clusters
-    labels = dbscan.fit_predict(data)
-
-    # Get unique cluster labels excluding noise (-1)
-    unique_labels = np.unique(labels[labels != -1])
-
-    # Compute the average LAB color of each cluster
-    cluster_averages = {}
-    for unique_label in unique_labels:
-        # Get indices of points belonging to the cluster
-        cluster_indices = np.where(labels == unique_label)[0]
-        # Get LAB colors of points in the cluster
-        cluster_lab_colors = data[cluster_indices]
-        # Compute the average LAB color of the cluster
-        average_lab_color = np.mean(cluster_lab_colors, axis=0)
-        cluster_averages[unique_label] = average_lab_color
-
-    # print(cluster_averages)
-    return cluster_averages
-
-
-def get_rgb_clusters_using_dbscan(clusters_as_lab):
-    """
-    Returns a list of RGB color triples corresponding to the color clusters. Found using DBSCAN.
-    :param clusters_as_lab: The clusters in LAB format.
-    :return: The list of RGB color triples corresponding to the color clusters.
-    """
-
-    # Extract LAB color triples from the dictionary
-    lab_colors = list(clusters_as_lab.values())
-
-    # Convert the list of LAB colors to a NumPy array
-    lab_array = np.array(lab_colors, dtype=np.float64)
-
-    # Ensure the array is 3-dimensional with shape (1, N, 3) for skimage compatibility
-    lab_array = lab_array[np.newaxis, :, :]
-
-    # Convert the LAB array to an RGB array using skimage
-    rgb_array = color.lab2rgb(lab_array)[0]
-
-    # Scale the RGB values to the range [0, 255]
-    rgb_array = (rgb_array * 255).astype(np.uint8)
-
-    # Convert the RGB array back to a list of tuples
-    rgb_colors = [tuple(rgb) for rgb in rgb_array]
-
-    return rgb_colors
 
 
 ########################################################################################################################
@@ -265,31 +192,6 @@ def get_small_image(big_image, max_side_length=300):
     return cv2.resize(big_image, (new_width, new_height))
 
 
-def get_small_image_as_pillow(big_image, max_side_length=200):
-    """Resize a given image to fit within a maximum side length while preserving aspect ratio
-
-    :param big_image: the image to be resized
-    :param max_side_length: the maximum size of a side 
-       
-    :return: the resized image
-    """
-
-    # get height and width
-    width, height = big_image.size
-
-    # Determine the scaling factor
-    if width > height:
-        scale_factor = max_side_length / width
-    else:
-        scale_factor = max_side_length / height
-
-    # Calculate the new dimensions
-    new_width = int(width * scale_factor)
-    new_height = int(height * scale_factor)
-
-    # Resize 
-    return big_image.resize((new_width, new_height))
-
 
 def get_image(image_file_path):
     """Read an image from the specified file path and convert it from BGR to RGB color space.
@@ -300,17 +202,6 @@ def get_image(image_file_path):
     """
     raw_image = cv2.imread(image_file_path)
     return cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB)
-
-
-def get_image_as_pillow(image_file_path):
-    """Open image from the specified file path
-
-    :param image_file path: the file path of the image
-       
-    :return: the loaded image as a Pillow Image object
-    """
-
-    return Image.open(image_file_path)
 
 
 ########################################################################################################################
@@ -357,22 +248,14 @@ def visualize_stacked_images(big_image, clusters, contrast):
 
 ### Get image(s)
 img_path = "VGM_Package2024007_TUE_XrfExplorer2_Roulin_V20240424/196_1989_RGB.tif"
-img_pillow = get_image_as_pillow(img_path)
-small_image_pillow = get_small_image_as_pillow(img_pillow, 200)
 img = get_image(img_path)
-small_image = get_small_image(img, 400)
+small_image = get_small_image(img, 300)
 
-###
-# Start timer
-start = time.time()
-
-k = 50
+k = 40
 color_similarity_threshold = 6
-color_merging_threshold = 1
+color_merging_threshold = 4
 
 # Get clusters
-# cluster = get_clusters_using_dbscan(small_image_pillow, eps=2, min_samples=30)
-# rgbClusters = get_rgb_clusters_using_dbscan(cluster)
 label, rgbClusters = get_clusters_using_k_means(small_image, nr_of_attempts=20, k=k)
 
 # merge similar clusters
@@ -381,26 +264,14 @@ newClusters = merge_similar_colors(rgbClusters, color_merging_threshold)
 # map clusters with bitmask
 cluster_res, bitmask = get_pixels_in_clusters(img, newClusters, color_similarity_threshold)
 
-
-
-# End timer
-end = time.time()
-print("Time taken: " + str(end - start))
-###
-
-visualize_clusters(small_image, rgbClusters)
+#visualize_clusters
 visualize_clusters(small_image, newClusters)
 
-## Visualize clusters
-# for c in rgbClusters:
+# for c in newClusters:
 for i in range(len(newClusters)):
 
     c = newClusters[i]
     # Create a colormap with cluster color
     specific_color = [c[0]/255, c[1]/255, c[2]/255, 1]
 
-    # Plot 
-    visualize_stacked_images(img, cluster_res[i], 0.8)
-    plt.title(str(c), color = specific_color)
-    plt.show()
 
