b40afa4e6ee47abb11245d8e738066ad7261cf28
Dirk <d.burgers@student.tue.nl>
2024-05-24 14:30:53 +0200

    Function to generate that middle picture.

--- xrf_explorer/server/dim_reduction/general.py
+++ xrf_explorer/server/dim_reduction/general.py
@@ -1,6 +1,6 @@
 import logging 
 
-from os.path import isfile
+from os.path import isfile, abspath
 from pathlib import Path
 
 import numpy as np
@@ -58,3 +58,55 @@ def get_registered_painting_image(type: str, config_path: str = "config/backend.
     pixels_of_image: np.ndarray = imageio.imread(path_to_image)
 
     return pixels_of_image
+
+
+def get_image_of_indices_to_embedding(config_path: str = "config/backend.yml"):
+    """Creates the image for lasso selection that decodes to which points in the embedding
+    the pixels of the elemental data cube are mapped. Uses the current embedding and indices
+    to create the image.
+
+    :param config_path: Path to the backend config file
+    :return: True if the image was created successfully, otherwise False.
+    """
+
+    # load backend config
+    backend_config: dict = load_yml(config_path)
+    if not backend_config:  # config is empty
+        return False
+
+    # Load the elemental data cube
+    elemental_cube: np.ndarray | None = get_elemental_data_cube(config_path=config_path)
+    if elemental_cube is None:
+        return False
+
+    # Load data indices and embedded data
+    dr_folder: str = backend_config['dim-reduction']['folder']
+
+    # Load the file embedding.npy
+    try:
+        indices: np.ndarray = np.load(Path(abspath(dr_folder), 'indices.npy'))
+        embedding: np.ndarray = np.load(Path(abspath(dr_folder), 'embedded_data.npy'))
+    except OSError as e:
+        LOG.error(f"Failed to load indices and/or embedding data. Error: {e}")
+        return False
+
+    # Get min and max values
+    xmin, ymin = np.min(embedding, axis=0)
+    xmax, ymax = np.max(embedding, axis=0)
+
+    # Normalize values to be in the range [0, 255]
+    embedding[:, 0] = np.interp(embedding[:, 0], (xmin, xmax), (0, +255))
+    embedding[:, 1] = np.interp(embedding[:, 1], (ymin, ymax), (0, +255))
+
+    # Initialize new image
+    newimage = np.zeros((elemental_cube.shape[0], elemental_cube.shape[1], 3), dtype=np.uint8)
+    
+    # Fill pixels
+    newimage[indices[:, 0], indices[:, 1], 0] = embedding[:, 0]
+    newimage[indices[:, 0], indices[:, 1], 1] = embedding[:, 1]
+    newimage[indices[:, 0], indices[:, 1], 2] = 255
+
+    # Create and save the image
+    imageio.imwrite(Path(abspath(dr_folder), 'image_index_to_embedding.npy'), newimage)
+
+    return True
