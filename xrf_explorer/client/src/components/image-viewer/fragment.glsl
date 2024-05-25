1f97a2a1d234a6f8179764d496b7faab99db4ad6
Lotte Lakeman <lottelakeman2001@gmail.com>
2024-05-25 13:27:55 +0200

    Adds functionality for the contrast slider

--- xrf_explorer/client/src/components/image-viewer/fragment.glsl
+++ xrf_explorer/client/src/components/image-viewer/fragment.glsl
@@ -51,12 +51,12 @@ void main() {
     gl_FragColor = transparent;
   }
 
-  // // Apply contrast adjustment
-  //   gl_FragColor.rgb = ((gl_FragColor.rgb - 0.5) * max(uContrast, 0.0)) + 0.5;
+  // Apply contrast adjustment
+  gl_FragColor.rgb = ((gl_FragColor.rgb - 0.5) * max(uContrast, 0.0)) + 0.5;
 
-  //   // Apply saturation adjustment
-  //   gl_FragColor.rgb = adjustSaturation(gl_FragColor.rgb, uSaturation);
+  // // Apply saturation adjustment
+  // gl_FragColor.rgb = adjustSaturation(gl_FragColor.rgb, uSaturation);
 
-    // Apply opacity
-    gl_FragColor = vec4(gl_FragColor.xyz, gl_FragColor.w * uOpacity);
+  // Apply opacity
+  gl_FragColor = vec4(gl_FragColor.xyz, gl_FragColor.w * uOpacity);
 }
