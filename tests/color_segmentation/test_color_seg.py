025e6bbf0e2e62606727c48b312aea2a42442edd
diego <driverag22@gmail.com>
2024-05-26 15:37:03 +0200

    Added test for get_pixels_in_clusters function

--- tests/color_segmentation/test_color_seg.py
+++ tests/color_segmentation/test_color_seg.py
@@ -5,7 +5,7 @@ import numpy as np
 
 sys.path.append('.')
 
-from xrf_explorer.server.color_seg import get_image, get_clusters_using_k_means, merge_similar_colors
+from xrf_explorer.server.color_seg import get_image, get_clusters_using_k_means, merge_similar_colors, get_pixels_in_clusters
 
 RESOURCES_PATH: Path = Path('tests', 'resources')
 
@@ -14,6 +14,7 @@ class TestColorSegmentation:
     BW_IMAGE_PATH: str = join(RESOURCES_PATH, Path('color_segmentation', 'black_and_white_image.png'))
 
     def test_get_clusters_using_k_means(self):
+        assert True
         # Set-up
         small_image: str = get_image(self.BW_IMAGE_PATH)
         expected_result = np.array([
@@ -36,3 +37,19 @@ class TestColorSegmentation:
         # Verify
         assert len(expected_result) == length
         assert np.array_equal(result, expected_result) or np.array_equal(result, expected_result2)
+
+    def test_get_pixels_in_clusters(self):
+        image: str = get_image(self.BW_IMAGE_PATH)
+        clusters = np.array([
+            [255, 255, 255],
+            [0, 0, 0]
+        ])
+        white_cluster = np.zeros((100, 100), dtype=int)
+        white_cluster[:, 50:] = 255
+        black_cluster = np.zeros((100, 100), dtype=int)
+        black_cluster[:, :50] = 255
+
+        bitmask = get_pixels_in_clusters(image, clusters, 10)
+
+        assert np.array_equal(white_cluster, bitmask[0])
+        assert np.array_equal(black_cluster, bitmask[1])
