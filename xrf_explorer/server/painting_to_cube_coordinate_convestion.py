ade8f634776b713d7d7832b21cd289cc7054b4db
Ivan Ivanov <ivanivanov.jsg@gmail.com>
2024-05-30 08:05:30 +0200

    Code refactor and the mapped points are now rounded

--- xrf_explorer/server/painting_to_cube_coordinate_convestion.py
+++ xrf_explorer/server/painting_to_cube_coordinate_convestion.py
@@ -1,67 +1,50 @@
 from xrf_explorer.server.file_system.elemental_cube import get_elemental_data_cube
 import json
-from os import getcwd
 from cv2 import imread
+import numpy as np
 from xrf_explorer.server.file_system.from_dms import (
     get_elemental_datacube_dimensions_from_dms,
 )
 
 
-def slicing(data_cube, coordinates):
-    coord1, coord2 = coordinates
-    x1, y1 = coord1
-    x2, y2 = coord2
-
-    # Corner case 1: coord dimensions are right: do not surpass elemental cube maximum
-    # shape of datacube: (c, h, w)
-    c, h, w = data_cube.shape
-    # if (y1<0) or (y2<0) or (h<y1) or (h<y2) or (x1<0) or (x2<0) or (w<x1) or (w<x2):
-    #     return []
-
-    # Corner case 2: line instead of rectangle
-    # if (x1 == x2) or (y1 == y2):
-    #     return []
+def get_sliced_data_cube(data_cube, selection_coord_1, selection_coord_2):
+    x1, y1 = selection_coord_1
+    x2, y2 = selection_coord_2
 
     # We implement the case distinction
-    if (x1 < x2) and (y1 < y2):
-        return data_cube[:, y1:y2, x1:x2]
+    x_min, x_max = sorted([x1, x2])
+    y_min, y_max = sorted([y1, y2])
 
-    elif (x1 < x2) and (y2 < y1):
-        return data_cube[:, y2:y1, x1:x2]
-
-    elif (x2 < x1) and (y1 < y2):
-        return data_cube[:, y1:y2, x2:x1]
-
-    else:
-        return data_cube[:, y2:y1, x2:x1]
+    return data_cube[:, y_min:y_max, x_min:x_max]
 
 
 def get_cube_coordinates(
-    rectangle_points_on_base_image: tuple[tuple[int, int], tuple[int, int]],
+    selection_coord_1: tuple[int, int],
+    selection_coord_2: tuple[int, int],
     base_img_width: int,
     base_img_height: int,
     cube_width: int,
     cube_height: int,
-):
-    point_1, point_2 = rectangle_points_on_base_image
-
-    x_1, y_1 = point_1
-    x_2, y_2 = point_2
+) -> tuple[tuple[int, int], tuple[int, int]]:
+    x_1, y_1 = selection_coord_1
+    x_2, y_2 = selection_coord_2
 
     ratio_img_cube_width = base_img_width / cube_width
     ratio_img_cube_height = base_img_height / cube_height
 
-    x_1_new = x_1 * ratio_img_cube_width
-    x_2_new = x_2 * ratio_img_cube_width
-    y_1_new = y_1 * ratio_img_cube_height
-    y_2_new = y_2 * ratio_img_cube_height
+    x_1_new = round(x_1 * ratio_img_cube_width)
+    x_2_new = round(x_2 * ratio_img_cube_width)
+    y_1_new = round(y_1 * ratio_img_cube_height)
+    y_2_new = round(y_2 * ratio_img_cube_height)
 
-    return ((x_1_new, y_1_new), (x_2_new, y_2_new))
+    return (x_1_new, y_1_new), (x_2_new, y_2_new)
 
 
 def get_selected_data_cube(
-    data_source_name: str, coordinates: tuple[tuple[int, int], tuple[int, int]]
-):
+    data_source_name: str,
+    selection_coord_1: tuple[int, int],
+    selection_coord_2: tuple[int, int],
+) -> np.ndarray:
     data_source_dir = f"xrf_explorer/server/data/{data_source_name}"
 
     file = open(f"{data_source_dir}/workspace.json")
@@ -78,6 +61,10 @@ def get_selected_data_cube(
 
     data_cube = get_elemental_data_cube(cube_name)
 
-    return slicing(data_cube, coordinates)
+    selection_coord_1_cube, selection_coord_2_cube = get_cube_coordinates(
+        selection_coord_1, selection_coord_2, img_w, img_h, cube_w, cube_h
+    )
 
-    mapped_coordinates = get_cube_coordinates(coordinates, img_w, img_h, cube_w, cube_h)
+    return get_sliced_data_cube(
+        data_cube, selection_coord_1_cube, selection_coord_2_cube
+    )
