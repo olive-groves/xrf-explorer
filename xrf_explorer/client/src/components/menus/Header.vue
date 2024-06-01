9f5dbc30be41c62c1703f6d6dbb395d3698cfa44
Jan Bulthuis <jbulthuis03@gmail.com>
2024-06-01 17:53:13 +0200

    Renamed TitleMenu to MainMenu

--- xrf_explorer/client/src/components/menus/Header.vue
+++ xrf_explorer/client/src/components/menus/Header.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { Menubar } from "@/components/ui/menubar";
-import { ExportMenu, FileMenu, TitleMenu, WindowMenu } from ".";
+import { ExportMenu, FileMenu, MainMenu, WindowMenu } from ".";
 </script>
 
 <template>
   <Menubar class="m-0 h-min w-full justify-between rounded-none border-0 border-b">
     <div class="flex">
-      <TitleMenu />
+      <MainMenu />
       <FileMenu />
       <WindowMenu />
     </div>
--- xrf_explorer/client/src/components/menus/index.ts
+++ xrf_explorer/client/src/components/menus/index.ts
@@ -2,5 +2,5 @@ export { default as Header } from "./Header.vue";
 export { default as BaseContextMenu } from "./BaseContextMenu.vue";
 export { default as ExportMenu } from "./ExportMenu.vue";
 export { default as FileMenu } from "./FileMenu.vue";
-export { default as TitleMenu } from "./TitleMenu.vue";
+export { default as MainMenu } from "./MainMenu.vue";
 export { default as WindowMenu } from "./WindowMenu.vue";
