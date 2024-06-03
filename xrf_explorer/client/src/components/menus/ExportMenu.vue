76e0226c900289eade0811b0eaa056e4f1a94ca7
Jan Bulthuis <jbulthuis03@gmail.com>
2024-06-03 16:57:34 +0200

    Updated export menu

--- xrf_explorer/client/src/components/menus/ExportMenu.vue
+++ xrf_explorer/client/src/components/menus/ExportMenu.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { MenubarMenu, MenubarTrigger, MenubarContent, MenubarItem } from "@/components/ui/menubar";
-import { exportToImage, exportableElements } from "@/lib/export";
+import { datasource } from "@/lib/appState";
+import { exportElement, exportScene, exportableElements } from "@/lib/export";
 import { sentenceCase, snakeCase } from "change-case";
 </script>
 
@@ -8,11 +9,12 @@ import { sentenceCase, snakeCase } from "change-case";
   <MenubarMenu>
     <MenubarTrigger> Export </MenubarTrigger>
     <MenubarContent>
-      <MenubarItem disabled v-if="Object.keys(exportableElements).length <= 0">Nothing to export</MenubarItem>
+      <MenubarItem disabled v-if="!datasource">Nothing to export</MenubarItem>
+      <MenubarItem v-if="datasource" @click="() => exportScene()"> Painting </MenubarItem>
       <MenubarItem
         v-for="name in Object.keys(exportableElements)"
         :key="name"
-        @click="() => exportToImage(snakeCase(name), exportableElements[name])"
+        @click="() => exportElement(snakeCase(name), exportableElements[name])"
       >
         {{ sentenceCase(name) }}
       </MenubarItem>
