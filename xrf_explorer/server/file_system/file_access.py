f9365a1e65892fdb9117a7cae4233bbae7c6ba2b
Ivan Ivanov <ivanivanov.jsg@gmail.com>
2024-06-04 11:31:21 +0200

    Fixed type problems of some file_access.py helper functions and added more error handling

--- xrf_explorer/server/file_system/file_access.py
+++ xrf_explorer/server/file_system/file_access.py
@@ -1,8 +1,8 @@
-#module responsible for giving the different paths corresponding to the files of a data source
+# module responsible for giving the different paths corresponding to the files of a data source
 
 import logging
 
-from os.path import isfile, join
+from os.path import isfile, join, exists
 from pathlib import Path
 
 import json
@@ -14,9 +14,12 @@ from xrf_explorer.server.file_system.config_handler import load_yml
 LOG: logging.Logger = logging.getLogger(__name__)
 BACKEND_CONFIG: dict = load_yml("config/backend.yml")
 
-def get_elemental_cube_name(data_source: str, config_path: str = "config/backend.yml") -> str:
-    """Get the location of the elemental cube file of a given datasource
-    
+
+def get_elemental_cube_name(
+    data_source: str, config_path: str = "config/backend.yml"
+) -> str | None:
+    """Get the location of the elemental cube file of a given datasource.
+
     :param datasource: Name of the datasource.
     :param config_path: Path to the backend config file.
     :return: Path string pointing to the elemental cube location.
@@ -25,57 +28,76 @@ def get_elemental_cube_name(data_source: str, config_path: str = "config/backend
     backend_config: dict = load_yml(config_path)
     if not backend_config:  # config is empty
         LOG.error("Config is empty")
-        return ""
-    
-    data_source_dir = join(Path(backend_config["uploads-folder"]), data_source, "workspace.json")
+        return None
+
+    data_source_dir = join(
+        Path(backend_config["uploads-folder"]), data_source, "workspace.json"
+    )
     try:
-        with open(data_source_dir, 'r') as workspace:
+        with open(data_source_dir, "r") as workspace:
             data_json: str = workspace.read()
             data = json.loads(data_json)
             elemental_cube_name: str = data["elementalCubes"][0]["dataLocation"]
     except OSError as err:
         LOG.error("Error while getting elemental cube name: {%s}", err)
-        return 400
-    
+        return None
+
     return elemental_cube_name
 
-def get_elemental_cube_path(data_source: str, config_path: str = "config/backend.yml") -> str:
+
+def get_elemental_cube_path(
+    data_source: str, config_path: str = "config/backend.yml"
+) -> str | None:
     """Get the path to the elemental data cube of a data source.
 
-    :param data_source: Name of the data source.
+    :param data_source: Name of the data source folder.
     :param config_path: Path to the backend config file.
     :return: Path to the elemental data cube.
     """
     # load backend config
     backend_config: dict = load_yml(config_path)
     if not backend_config:  # config is empty
-        LOG.error("Config is empty")
-        return ""
+        LOG.error("Config is empty.")
+        return None
 
-    filename: str = get_elemental_cube_name(data_source, config_path)
+    if not exists(join(backend_config["uploads-folder"], data_source)):
+        LOG.error("Data source folder name does not exist.")
+        return None
+
+    filename: str | None = get_elemental_cube_name(data_source, config_path)
+
+    if filename is None:
+        LOG.error("An error occured while trying to find elemental cube name.")
+        return None
+
     path: str = join(Path(backend_config["uploads-folder"]), data_source, filename)
-    
-    #raise error is the path does not exist
+
+    # raise error is the path does not exist
     if not isfile(path):
         raise OSError("Provided datasource does not have an elemental cube file")
 
     return path
 
-def get_raw_rpl_names(data_source: str, config_path: str = "config/backend.yml") -> tuple[str, str]:
+
+def get_raw_rpl_names(
+    data_source: str, config_path: str = "config/backend.yml"
+) -> tuple[str, str]:
     """Get the name of the raw data file (.raw) and the .rpl file of a given datasource
-    
+
     :param datasource: Name of the datasource.
     :return: Names of the raw data and rpl files.
     """
-     # load backend config
+    # load backend config
     backend_config: dict = load_yml(config_path)
     if not backend_config:  # config is empty
         LOG.error("Config is empty")
         return np.empty(0)
-    
-    data_source_dir: str = join(Path(backend_config["uploads-folder"]), data_source, "workspace.json")
+
+    data_source_dir: str = join(
+        Path(backend_config["uploads-folder"]), data_source, "workspace.json"
+    )
     try:
-        with open(data_source_dir, 'r') as workspace:
+        with open(data_source_dir, "r") as workspace:
             data_json: str = workspace.read()
             data = json.loads(data_json)
             raw_data_name: str = data["spectralCubes"][0]["rawLocation"]
@@ -83,12 +105,15 @@ def get_raw_rpl_names(data_source: str, config_path: str = "config/backend.yml")
     except OSError as err:
         LOG.error("Error while getting raw and rpl file locations: {%s}", err)
         return 400
-    
+
     return raw_data_name, rpl_name
 
-def get_raw_rpl_paths(data_source: str, config_path: str = "config/backend.yml") -> tuple[str, str]:
+
+def get_raw_rpl_paths(
+    data_source: str, config_path: str = "config/backend.yml"
+) -> tuple[str, str]:
     """Get the paths to the raw data file (.raw) and the .rpl file of a given datasource
-    
+
     :param datasource: Name of the datasource.
     :return: Paths to the raw data and rpl files.
     """
@@ -97,15 +122,16 @@ def get_raw_rpl_paths(data_source: str, config_path: str = "config/backend.yml")
     if not backend_config:  # config is empty
         LOG.error("Config is empty")
         return np.empty(0)
-    
+
     raw_name, rpl_name = get_raw_rpl_names(data_source)
     # get the path to the raw data in the server
     path_to_raw: str = join(backend_config["uploads-folder"], data_source, raw_name)
-    
+
     # get the path to the rpl file in the server
     path_to_rpl: str = join(backend_config["uploads-folder"], data_source, rpl_name)
-    
+
     return path_to_raw, path_to_rpl
+
 
 def parse_rpl(path: str) -> dict:
     """Parse the rpl file of a data source as a dictionary, containing the following info:
@@ -117,28 +143,29 @@ def parse_rpl(path: str) -> dict:
         - data type
         - byte order
         - record by
-        
+
     :param path: path to the rpl file
     :return: Dictionary containing the attributes' name and value
     """
-    
+
     try:
-        with open(path, 'r') as in_file:
-            info = in_file.read().splitlines() #first split on linebreak
+        with open(path, "r") as in_file:
+            info = in_file.read().splitlines()  # first split on linebreak
     except OSError as err:
         LOG.error("error while reading rpl file: {%s}", err)
         return {}
-    
+
     map = {}
-    if info:    
+    if info:
         for line in info:
-            split = line.split() #split on whitespace
+            split = line.split()  # split on whitespace
             if len(split) == 2:
-                map[split[0].strip()] = split[1].strip() #add tuple to dictionary
+                map[split[0].strip()] = split[1].strip()  # add tuple to dictionary
     else:
         LOG.error("Error while parsing rpl file: file empty")
-               
+
     return map
+
 
 def get_rgb_name(data_source: str, config_path: str = "config/backend.yml") -> str:
     """Get the name of the rgb image of a given datasource
@@ -153,9 +180,11 @@ def get_rgb_name(data_source: str, config_path: str = "config/backend.yml") -> s
         LOG.error("Config is empty")
         return ""
 
-    data_source_dir = join(Path(backend_config["uploads-folder"]), data_source, "workspace.json")
+    data_source_dir = join(
+        Path(backend_config["uploads-folder"]), data_source, "workspace.json"
+    )
     try:
-        with open(data_source_dir, 'r') as workspace:
+        with open(data_source_dir, "r") as workspace:
             data_json: str = workspace.read()
             data = json.loads(data_json)
             rgb_name: str = data["baseImage"]["imageLocation"]
@@ -164,6 +193,7 @@ def get_rgb_name(data_source: str, config_path: str = "config/backend.yml") -> s
         return 400
 
     return rgb_name
+
 
 def get_rgb_path(data_source: str, config_path: str = "config/backend.yml") -> str:
     """Get the path to rgb image of a data source.
@@ -181,8 +211,9 @@ def get_rgb_path(data_source: str, config_path: str = "config/backend.yml") -> s
     filename: str = get_rgb_name(data_source, config_path)
     path: str = join(Path(backend_config["uploads-folder"]), data_source, filename)
 
-    #raise error is the path does not exist
+    # raise error is the path does not exist
     if not isfile(path):
         raise OSError("Provided datasource does not have an elemental cube file")
 
     return path
+
