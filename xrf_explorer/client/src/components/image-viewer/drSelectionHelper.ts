081ff75ce87eb897bde8a3b9bb7f0fb53bd2c9b2
IMaxiM03I <maxilea202@gmail.com>
2024-06-07 18:58:39 +0200

    load middle image into shader

--- xrf_explorer/client/src/components/image-viewer/drSelectionHelper.ts
+++ xrf_explorer/client/src/components/image-viewer/drSelectionHelper.ts
@@ -18,7 +18,6 @@ let embeddingWidth: number = -1;
 let embeddingHeight: number = -1;
 let imageWidth: number = -1;
 let imageHeight: number = -1;
-const middleImagePath = ref();
 // list of pixels in the embedding scaled down to 256x256, pixels that are selected have a color, others have opacity 0
 const layerData: Uint8Array = new Uint8Array(256 * 256 * 4);
 const layerTexture: DataTexture = createDataTexture(layerData, imageWidth, imageHeight);
@@ -53,9 +52,6 @@ async function onSelectionUpdate(newSelection: DimensionalityReductionSelection 
     embeddingWidth = width;
     embeddingHeight = height;
     updateBitmask(newSelection);
-
-    // map the selection to the image viewer
-    await getMiddleImage();
 
     // update the layer to display the selection
     updateLayer(newSelection.points.length);
@@ -193,18 +189,24 @@ async function getMiddleImage() {
         return;
     }
 
-    // store the middle image path
-    middleImagePath.value = URL.createObjectURL(data.value).toString();
     console.info("Loaded middle image for DR selection.");
+
+    return URL.createObjectURL(data.value).toString();  // return the path
 }
 
 /**
  *
  * @param nPointsSelected
  */
-function updateLayer(nPointsSelected: number): void {
+async function updateLayer(nPointsSelected: number): void {
     if (layerGroups.value.selection != undefined) {
         const layer: Layer = layerGroups.value.selection.layers.filter(layer => layer.image == "dr_selection")[0];
+        const middleImagePath: string | undefined = await getMiddleImage();
+        if (middleImagePath == undefined) {
+            console.error("Failed to load DR middle image.");
+            return;
+        }
+        layer.image = middleImagePath;
 
         if (layer.mesh == undefined && nPointsSelected > 0)        // layer was disposed of, load it again
             loadLayer(layer);
