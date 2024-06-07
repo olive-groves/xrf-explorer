de4f8d65b995d8e97cef830609923c80d9ce0fc5
Adrien Verriele <a.a.m.verriele@student.tue.nl>
2024-06-07 18:21:14 +0200

    get_selected_data_cube also handles raw data

--- xrf_explorer/server/image_to_cube_selection/image_to_cube_selection.py
+++ xrf_explorer/server/image_to_cube_selection/image_to_cube_selection.py
@@ -3,10 +3,13 @@ from xrf_explorer.server.file_system.file_access import (
     get_elemental_cube_path,
     get_base_image_path,
     get_cube_recipe_path,
+    get_raw_rpl_paths
 )
 from cv2 import imread
 import numpy as np
 import logging
+
+from xrf_explorer.server.spectra import get_raw_data
 
 LOG: logging.Logger = logging.getLogger(__name__)
 
@@ -63,6 +66,7 @@ def get_scaled_cube_coordinates(
 
 def get_selected_data_cube(
     data_source_folder: str,
+    cube_type: str,
     selection_coord_1: tuple[int, int],
     selection_coord_2: tuple[int, int],
 ) -> np.ndarray | None:
@@ -74,6 +78,7 @@ def get_selected_data_cube(
     scaled to match the data cube's dimensions.
 
     :param data_source_folder: The data source folder name.
+    :param cube_type: The type of the cube the selection is made on. "elemental" for the elemental cube, "raw" for the raw data cube.
     :param selection_coord_1: The first coordinate tuple (x1, y1), representing one corner of the rectangular
     region in the base image.
     :param selection_coord_2: The second coordinate tuple (x2, y2), representing the opposite corner of the
@@ -81,21 +86,32 @@ def get_selected_data_cube(
     :return: A 2D array where the rows represent the selected pixels from the data cube image and the columns
     represent their elemental map values.
     """
-    cube_dir: str | None = get_elemental_cube_path(data_source_folder)
+
+    match cube_type:
+        case "elemental":
+            cube_dir: str | None = get_elemental_cube_path(data_source_folder)
+        case "raw":
+            cube_dir: str | None = get_raw_rpl_paths(data_source_folder)[0]
 
     if cube_dir is None:
-        LOG.error(f"Data source directory {data_source_folder} does not exist.")
+        LOG.error(f"Data source directory {
+                  data_source_folder} does not exist.")
         return None
 
     base_img_dir: str | None = get_base_image_path(data_source_folder)
 
     if base_img_dir is None:
         LOG.error(
-            f"Error occured while retrieving the path fo the base image of {data_source_folder}"
+            f"Error occured while retrieving the path fo the base image of {
+                data_source_folder}"
         )
         return None
 
-    data_cube: np.ndarray = get_elemental_data_cube(cube_dir)
+    match cube_type:
+        case "elemental":
+            data_cube: np.ndarray = get_elemental_data_cube(cube_dir)
+        case "raw":
+            data_cube: np.ndarray = get_raw_data(data_source_folder)
 
     img_h, img_w, _ = imread(base_img_dir).shape
     cube_h, cube_w = data_cube.shape[1], data_cube.shape[2]
@@ -112,15 +128,15 @@ def get_selected_data_cube(
         x2, y2 = selection_coord_2_cube
 
         mask: np.ndarray = np.zeros((cube_h, cube_w), dtype=bool)
-        mask[y1 : y2 + 1, x1 : x2 + 1] = True
+        mask[y1: y2 + 1, x1: x2 + 1] = True
 
         # Note: Using selection with a boolean mask for a simple rectangular selection is not
-        #       the best choice for performance, but the mask simplifies things, since it can be 
+        #       the best choice for performance, but the mask simplifies things, since it can be
         #       used for all kinds of selections to be implemented in the future.
         #       Also the performance decrease should be less than tenth of a second in most cases.
         return extract_selected_data(data_cube, mask)
     else:
-        # If the data cube has recipe, deregister the selection coordinates so they correctly represent 
+        # If the data cube has recipe, deregister the selection coordinates so they correctly represent
         # the selected area on the data cube
         LOG.warn("Deregistration logic not yet implemented!")
         return np.array([])
