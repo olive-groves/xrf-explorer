Fixed selection average computations, and verified they are correct

xrf_explorer/server/file_system/elemental_cube.py
@@ -185,24 +185,28 @@ def get_element_averages(path: str) -> list[dict[str, str | float]]:
     image_cube: np.ndarray = normalize_ndarray_to_grayscale(raw_cube)
 
     # Calculate the average composition of the elements
-    composition: list[dict[str,  str | float]] = get_element_averages_rect(image_cube, names)
-
-    return composition
-
-
-def get_element_averages_rect(image_cube: np.ndarray, names: list[str]) -> list[dict[str, str | float]]:
-    """Get the names and averages of the elements present in (a subarea of) the painting.
-    :param image_cube: The normalized (subarea of) the painting.
-    :param names: The names of the elements present in the painting.
-    :return: List of the names and average composition of the elements.
-    """
     averages: np.ndarray = np.mean(image_cube, axis=(1, 2))
 
     # Create a list of dictionaries with the name and average composition of the elements
     composition: list[dict[str,  str | float]] = \
-        [{"name": names[i], "average": averages[i]} for i in range(averages.size)]
+        [{"name": names[i], "average": float(averages[i])} for i in range(averages.size)]
 
-    LOG.info("Calculated the average composition of the elements.")
+    return composition
+
+
+def get_element_averages_selection(selection: np.ndarray, names: list[str]) -> list[dict[str, str | float]]:
+    """Get the names and averages of the elements present in (a subarea of) the painting.
+    :param selection: A 2D array where the rows represent the selected pixels from the data cube image and the columns
+    represent their elemental map values.
+    :param names: The names of the elements present in the painting.
+    :return: List of the names and average composition of the elements.
+    """
+    # Calculate the average composition of each element in the selection
+    averages: np.ndarray = np.mean(selection, axis=1)
+
+    # Create a list of dictionaries with the name and average composition of the elements
+    composition: list[dict[str,  str | float]] = \
+        [{"name": names[i], "average": float(averages[i])} for i in range(averages.size)]
 
     return composition
 

xrf_explorer/server/image_to_cube_selection/image_to_cube_selection.py
@@ -1,4 +1,6 @@
-from xrf_explorer.server.file_system.elemental_cube import get_elemental_data_cube
+from xrf_explorer.server.file_system.elemental_cube import (
+    get_elemental_data_cube, normalize_ndarray_to_grayscale
+)
 from xrf_explorer.server.file_system.file_access import (
     get_elemental_cube_path,
     get_base_image_path,
@@ -95,14 +97,15 @@ def get_selected_data_cube(
         )
         return None
 
-    data_cube: np.ndarray = get_elemental_data_cube(cube_dir)
+    raw_cube: np.ndarray = get_elemental_data_cube(cube_dir)
+    data_cube: np.ndarray = normalize_ndarray_to_grayscale(raw_cube)
 
     img_h, img_w, _ = imread(base_img_dir).shape
     cube_h, cube_w = data_cube.shape[1], data_cube.shape[2]
 
     cube_recipe_path: str | None = get_cube_recipe_path(data_source_folder)
 
-    if cube_recipe_path is None:
+    if (cube_recipe_path is None) or True:
         # If the data cube has no recipe, simply scale the selection coordinates to match the dimensions of the cube.
         selection_coord_1_cube, selection_coord_2_cube = get_scaled_cube_coordinates(
             selection_coord_1, selection_coord_2, img_w, img_h, cube_w, cube_h

xrf_explorer/server/routes.py
@@ -23,7 +23,7 @@ from xrf_explorer.server.file_system.data_listing import get_data_sources_names
 from xrf_explorer.server.file_system.elemental_cube import (
     get_short_element_names, get_element_averages,
     get_elemental_map, normalize_ndarray_to_grayscale,
-    get_element_averages_rect
+    get_element_averages_selection
 )
 from xrf_explorer.server.file_system.file_access import get_elemental_cube_path, get_raw_rpl_paths, get_base_image_name
 from xrf_explorer.server.image_register.register_image import load_points_dict
@@ -240,21 +240,20 @@ def list_element_averages_selection(data_source: str):
     )
 
     # get selection
-    selection_cube: np.ndarray | None = get_selected_data_cube(
+    selection: np.ndarray | None = get_selected_data_cube(
         data_source, top_left, bottom_right
     )
 
-    if not selection_cube:
+    if selection is None:
         return "Error occurred while getting selection from datacube", 500
 
     # get names
     names: list[str] = get_short_element_names(path)
 
     # get averages
-    composition: list[dict[str, str | float]] = get_element_averages_rect(
-        selection_cube, names
+    composition: list[dict[str, str | float]] = get_element_averages_selection(
+        selection, names
     )
-    LOG.info(composition)
 
     try:
         return json.dumps(composition)