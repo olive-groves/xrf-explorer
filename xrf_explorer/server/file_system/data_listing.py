8d40ba23dd071ebe1611ab62e44ef67b5d6e0814
Lotte Lakeman <lottelakeman2001@gmail.com>
2024-06-09 13:54:36 +0200

    Resolves PR comments on data_listing.py

--- xrf_explorer/server/file_system/data_listing.py
+++ xrf_explorer/server/file_system/data_listing.py
@@ -1,7 +1,6 @@
 import logging
 
 from os import listdir
-import os.path
 from os.path import isdir, isfile, join
 from pathlib import Path
 
@@ -24,11 +23,12 @@ def get_data_sources_names() -> list[str]:
         return []
 
     # Path to folder where files are stored
-    path = Path(backend_config['uploads-folder'])
+    path: str = backend_config['uploads-folder']
 
     # Return list of all data source names in the folder
     # The data source names are the names of the folders in the data folder that contain a workspace.json
-    folders: list[str] = [filename for filename in listdir(path) if isdir(join(path, filename)) and isfile(join(path, filename, "workspace.json"))]
+    folders: list[str] = [filename for filename in listdir(path) if isdir(join(path, filename)) 
+            and isfile(join(path, filename, "workspace.json"))]
 
     LOG.info(f"Successful. Data sources in folder: {folders}")
     return folders
@@ -36,20 +36,20 @@ def get_data_sources_names() -> list[str]:
 
 def get_data_source_files(data_source: str) -> list[str]:
     """Return a list of all the files stored in the folder belonging to a data source.
-Does not look at files in subdirectories.
+    Does not look at files in subdirectories.
 
     :param data_source: name of the data source
     :return: list of all files stored in the folder belonging to a data source
     """
 
     # load config
-    config: dict = get_config()
+    config: dict | None = get_config()
     if not config:
         LOG.error("Config is empty")
         return []
 
     # Path to folder where the files are stored
-    path = Path(join(config['uploads-folder'], data_source))
+    path = join(config['uploads-folder'], data_source)
 
     # Return list of all data source names in the folder
     # The data source names are the names of the folders in the data folder that contain a workspace.json
