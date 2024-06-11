b7d9f0429d1025f8cc652b14ceffeec5790ebf3e
Lotte Lakeman <lottelakeman2001@gmail.com>
2024-06-11 22:55:07 +0200

    Colors attempt

--- xrf_explorer/client/src/components/image-viewer/drSelectionHelper.ts
+++ xrf_explorer/client/src/components/image-viewer/drSelectionHelper.ts
@@ -237,7 +237,7 @@ export function updateMiddleImage(): void {
  * Create the first instance of the DR Selection layer and add it to the global group of layers.
  */
 export async function createDRSelectionLayer() {
-  setSelectionColor(hexToRgb(config.selectionTool.fill_color));
+  setSelectionColor(hexToRgb("hsl(var(--selection))"));
 
   const recipe = await getRecipe(`${config.api.endpoint}/${datasource.value}/data/recipe`);
   recipe.movingSize = await getDataSize();
