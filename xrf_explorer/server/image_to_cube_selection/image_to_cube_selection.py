d9e1fe4927d92510cd9d6528fec237847f376f08
Ivan Ivanov <ivanivanov.jsg@gmail.com>
2024-06-13 18:45:46 +0200

    Added missing types to image_to_cube_selection.py

--- xrf_explorer/server/image_to_cube_selection/image_to_cube_selection.py
+++ xrf_explorer/server/image_to_cube_selection/image_to_cube_selection.py
@@ -30,9 +30,9 @@ def perspective_transform_coord(coord: tuple[int, int], transform_matrix: np.nda
     :param transform_matrix: The transformation matrix.
     :return: A (x', y') perspective transformed coordinate.
     """
-    coord_correct_format = np.array([[[coord[0], coord[1]]]], dtype="float32")
+    coord_correct_format: np.ndarray = np.array([[[coord[0], coord[1]]]], dtype="float32")
 
-    perspective_transformed_point = perspectiveTransform(coord_correct_format, transform_matrix)
+    perspective_transformed_point: np.ndarray = perspectiveTransform(coord_correct_format, transform_matrix)
     return (
         (perspective_transformed_point[0][0][0]),
         (perspective_transformed_point[0][0][1]),
@@ -65,19 +65,27 @@ def deregister_coord(
     #       system.
 
     # First step: Reverse perspective transformation
+    src_points: np.ndarray
+    base_points: np.ndarray
     src_points, base_points = load_points(cube_recipe_path)
-    transform_matrix = getPerspectiveTransform(base_points, src_points)
-    x_perspective_reversed, y_perspective_reversed = perspective_transform_coord(coord, transform_matrix)
 
-    cube_height_scaled_before_pad, registered_uv_width_before_pad = compute_fitting_dimensions_by_aspect(
+    transform_matrix: np.ndarray = getPerspectiveTransform(base_points, src_points)
+    
+    x_perspective_reversed: float
+    y_perspective_reversed:float
+    x_perspective_reversed, y_perspective_reversed = perspective_transform_coord(coord, transform_matrix)
+    
+    cube_height_scaled_before_pad: int
+    cube_width_scaled_before_pad: int
+    cube_height_scaled_before_pad, cube_width_scaled_before_pad = compute_fitting_dimensions_by_aspect(
         cube_height, cube_width, base_img_height, base_img_width
     )
 
     # Second step: Reverse scaling
-    x_reversed_scaling = None
-    y_reversed_scaling = None
+    x_reversed_scaling: float
+    y_reversed_scaling: float
 
-    ratio_x = cube_width / registered_uv_width_before_pad
+    ratio_x = cube_width / cube_width_scaled_before_pad
     ratio_y = cube_height / cube_height_scaled_before_pad
 
     x_reversed_scaling = x_perspective_reversed * ratio_x
@@ -169,7 +177,11 @@ def get_selected_data_cube(
         return None
 
     data_cube: np.ndarray = get_elemental_data_cube(cube_dir)
-
+    
+    img_h: int
+    img_w: int
+    cube_h: int
+    cube_w: int
     img_h, img_w, _ = imread(base_img_dir).shape
     cube_h, cube_w = data_cube.shape[1], data_cube.shape[2]
 
@@ -177,10 +189,16 @@ def get_selected_data_cube(
 
     if cube_recipe_path is None:
         # If the data cube has no recipe, simply scale the selection coordinates to match the dimensions of the cube.
+        selection_coord_1_cube: tuple[int, int]
+        selection_coord_2_cube: tuple[int, int]
         selection_coord_1_cube, selection_coord_2_cube = get_scaled_cube_coordinates(
             selection_coord_1, selection_coord_2, img_w, img_h, cube_w, cube_h
         )
-
+        
+        x1: int
+        x2: int
+        y1: int
+        y2: int
         x1, y1 = selection_coord_1_cube
         x2, y2 = selection_coord_2_cube
 
@@ -195,33 +213,37 @@ def get_selected_data_cube(
     else:
         # If the data cube has recipe, deregister the selection coordinates so they correctly represent
         # the selected area on the data cube
+        x1: int
+        x2: int
+        y1: int
+        y2: int
         x1, y1 = selection_coord_1
         x2, y2 = selection_coord_2
 
         # Get all 4 points of the selection rectangle
-        p1 = (x1, y1)
-        p2 = (x1, y2)
-        p3 = (x2, y1)
-        p4 = (x2, y2)
+        p1: tuple[int, int] = (x1, y1)
+        p2: tuple[int, int] = (x1, y2)
+        p3: tuple[int, int] = (x2, y1)
+        p4: tuple[int, int] = (x2, y2)
 
         # Deregister to cube coordinates
         args = (cube_recipe_path, img_h, img_w, cube_h, cube_w)
-        p1_cube = deregister_coord(p1, *args)
-        p2_cube = deregister_coord(p2, *args)
-        p3_cube = deregister_coord(p3, *args)
-        p4_cube = deregister_coord(p4, *args)
+        p1_cube: tuple[int, int] = deregister_coord(p1, *args)
+        p2_cube: tuple[int, int] = deregister_coord(p2, *args)
+        p3_cube: tuple[int, int] = deregister_coord(p3, *args)
+        p4_cube: tuple[int, int] = deregister_coord(p4, *args)
 
-        cube_points = np.array([p1_cube, p2_cube, p3_cube, p4_cube])
-        mask = np.zeros((cube_h, cube_w), dtype=np.uint8)
+        cube_points: np.ndarray = np.array([p1_cube, p2_cube, p3_cube, p4_cube])
+        mask: np.ndarray = np.zeros((cube_h, cube_w), dtype=np.uint8)
 
         # Calculate the smallest convex set that contains all the points
         # The purpose of this is to order the points so they construct a polygon instead
         # of an hourglass figure
-        convex_hull = convexHull(cube_points)
+        convex_hull: np.ndarray = convexHull(cube_points)
 
         # Write 1's in the polygon area
         fillConvexPoly(mask, convex_hull, (1,))
 
-        mask = mask.astype(bool)
+        mask: np.ndarray = mask.astype(bool)
 
         return extract_selected_data(data_cube, mask)
