f2fb1ef03c23fbaa7e0d193bf2733f75e3823f41
Ivan Ivanov <ivanivanov.jsg@gmail.com>
2024-06-13 19:21:59 +0200

    Added missing types to test_image_to_cube_selection.py

--- tests/image_to_cube_selection/test_image_to_cube_selection.py
+++ tests/image_to_cube_selection/test_image_to_cube_selection.py
@@ -25,10 +25,10 @@ RESOURCES_PATH: str = join("tests", "resources")
 
 class TestImageToCubeSelection:
     CUSTOM_CONFIG_PATH: str = join(RESOURCES_PATH, "configs", "image_to_cube_selection.yml")
-    DATA_SOURCE_FOLDER_NAME = "Data_source"
-    SAMPLE_BASE_IMAGE_PATH = join(RESOURCES_PATH, "image_to_cube_selection", "rgb.tif")
-    SAMPLE_CUBE_IMG_PATH = join(RESOURCES_PATH, "image_to_cube_selection", "cube.tif")
-    SAMPLE_CUBE_RECIPE_PATH = join(RESOURCES_PATH, "image_to_cube_selection", "recipe_cube.csv")
+    DATA_SOURCE_FOLDER_NAME: str = "Data_source"
+    SAMPLE_BASE_IMAGE_PATH: str= join(RESOURCES_PATH, "image_to_cube_selection", "rgb.tif")
+    SAMPLE_CUBE_IMG_PATH: str = join(RESOURCES_PATH, "image_to_cube_selection", "cube.tif")
+    SAMPLE_CUBE_RECIPE_PATH: str = join(RESOURCES_PATH, "image_to_cube_selection", "recipe_cube.csv")
 
     @pytest.fixture(autouse=True)
     def setup_environment(self):
@@ -69,6 +69,8 @@ class TestImageToCubeSelection:
         cube_image_width: int = 5
 
         # execute
+        data_cube_output_1_actual: tuple[int, int]
+        data_cube_output_2_actual: tuple[int, int]
         data_cube_output_1_actual, data_cube_output_2_actual = get_scaled_cube_coordinates(
             RGB_POINT_1,
             RGB_POINT_2,
@@ -100,7 +102,9 @@ class TestImageToCubeSelection:
 
         if base_img_dir is None:
             pytest.fail("Base image directory is None")
-
+        
+        img_h: int
+        img_w: int
         img_h, img_w, _ = imread(base_img_dir).shape
 
         cube_img_w_ratio: float = cube_w / img_w
@@ -132,33 +136,42 @@ class TestImageToCubeSelection:
 
     # Return true if cube_coord_expected is within tolerance_pixels from the cube coordinate calculated by
     # deregister_coord.
-    def is_deregistration_correct(self, base_img_coord: tuple[int, int], cube_coord_expected, tolerance_pixels):
-        base_img = imread(self.SAMPLE_BASE_IMAGE_PATH)
-        cube_img = imread(self.SAMPLE_CUBE_IMG_PATH)
+    def is_deregistration_correct(
+        self,
+        base_img_coord: tuple[int, int],
+        cube_coord_expected: tuple[int, int],
+        tolerance_pixels: int,
+    ) -> bool:
+        base_img: np.ndarray = imread(self.SAMPLE_BASE_IMAGE_PATH)
+        cube_img: np.ndarray = imread(self.SAMPLE_CUBE_IMG_PATH)
 
+        base_h: int
+        base_w: int
+        cube_h: int
+        cube_w: int
         base_h, base_w, _ = base_img.shape
         cube_h, cube_w, _ = cube_img.shape
 
         args = (self.SAMPLE_CUBE_RECIPE_PATH, base_h, base_w, cube_h, cube_w)
         cube_coord_actual = deregister_coord(base_img_coord, *args)
 
-        euclidean_dist = (cube_coord_expected[0] - cube_coord_actual[0]) ** 2 + (
+        euclidean_dist: int = (cube_coord_expected[0] - cube_coord_actual[0]) ** 2 + (
             cube_coord_expected[1] - cube_coord_actual[1]
         ) ** 2
 
         return euclidean_dist <= tolerance_pixels
 
     def test_deregister_coord(self):
-        BASE_IMG_COORD_1 = (2046, 2691)
-        CUBE_COORD_EXPECTED_1 = (438, 522)
+        BASE_IMG_COORD_1: tuple[int, int] = (2046, 2691)
+        CUBE_COORD_EXPECTED_1: tuple[int, int] = (438, 522)
 
-        BASE_IMG_COORD_2 = (2531, 1773)
-        CUBE_COORD_EXPECTED_2 = (540, 327)
+        BASE_IMG_COORD_2: tuple[int, int] = (2531, 1773)
+        CUBE_COORD_EXPECTED_2: tuple[int, int] = (540, 327)
 
-        BASE_IMG_COORD_3 = (1020, 1933)
-        CUBE_COORD_EXPECTED_3 = (218, 360)
+        BASE_IMG_COORD_3: tuple[int, int] = (1020, 1933)
+        CUBE_COORD_EXPECTED_3: tuple[int, int] = (218, 360)
 
-        TOLERANCE_PIXELS = 20
+        TOLERANCE_PIXELS: int = 20
 
         assert self.is_deregistration_correct(BASE_IMG_COORD_1, CUBE_COORD_EXPECTED_1, TOLERANCE_PIXELS)
         assert self.is_deregistration_correct(BASE_IMG_COORD_2, CUBE_COORD_EXPECTED_2, TOLERANCE_PIXELS)
