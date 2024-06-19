677dcffe8ef292e8117a3dd496afeee796d86a34
Dirk <d.burgers@student.tue.nl>
2024-06-19 14:58:21 +0200

    Added docstrings to packages

--- xrf_explorer/server/process/spectra/__init__.py
+++ xrf_explorer/server/process/spectra/__init__.py
@@ -1,6 +1,7 @@
-# This module contains all functions related to the spectral chart
+"""This module handles everything related to the spectral chart."""
+
 from .spectra import (
     get_average_global, get_raw_data, get_average_selection, get_theoretical_data, bin_data
 )
 
-__all__ = ["get_average_global", "get_raw_data", "get_average_selection", "bin_data"]
+__all__ = ["get_average_global", "get_raw_data", "get_average_selection", "get_theoretical_data"]
