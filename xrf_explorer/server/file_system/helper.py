fe572cbe652d84402c58db9f93c550d1eabff868
IMaxi_M03I <maxilea202@gmail.com>
2024-06-19 14:31:01 +0200

    unused import

--- xrf_explorer/server/file_system/helper.py
+++ xrf_explorer/server/file_system/helper.py
@@ -2,8 +2,6 @@ from logging import Logger, getLogger
 from os.path import abspath
 from yaml import safe_load, YAMLError
 
-import numpy as np
-
 
 LOG: Logger = getLogger(__name__)
 APP_CONFIG: dict | None = None
