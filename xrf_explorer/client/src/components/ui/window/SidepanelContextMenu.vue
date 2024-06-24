988313f11951e32610e1c2a4676b2816a41d415f
Jan Bulthuis <jbulthuis03@gmail.com>
2024-06-24 13:21:02 +0200

    Flattened sidepanelcontextmenu

--- xrf_explorer/client/src/components/ui/window/SidepanelContextMenu.vue
+++ xrf_explorer/client/src/components/ui/window/SidepanelContextMenu.vue
@@ -1,11 +1,5 @@
 <script setup lang="ts">
-import {
-  ContextMenuItem,
-  ContextMenuSub,
-  ContextMenuSubTrigger,
-  ContextMenuSubContent,
-  ContextMenuCheckboxItem,
-} from "@/components/ui/context-menu";
+import { ContextMenuItem, ContextMenuSeparator, ContextMenuCheckboxItem } from "@/components/ui/context-menu";
 import { ResizablePanel } from "../resizable";
 import { WindowLocation, windowState } from "./state";
 import { toRef } from "vue";
@@ -23,29 +17,25 @@ const location = toRef(props, "location");
 </script>
 
 <template>
-  <ContextMenuItem v-if="panel?.isExpanded" @click="panel?.collapse"> Collapse sidebar </ContextMenuItem>
-  <ContextMenuItem v-else @click="panel?.expand">Expand sidebar</ContextMenuItem>
-  <ContextMenuSub>
-    <ContextMenuSubTrigger> Open window </ContextMenuSubTrigger>
-    <ContextMenuSubContent>
-      <ContextMenuCheckboxItem
-        v-for="window in windowState"
-        :key="window.id"
-        :checked="window.opened && window.location == location"
-        :disabled="window.disabled"
-        @update:checked="
-          {
-            if (window.opened && window.location == location) {
-              window.opened = false;
-            } else {
-              window.opened = true;
-              window.location = location;
-            }
-          }
-        "
-      >
-        {{ window.title }}
-      </ContextMenuCheckboxItem>
-    </ContextMenuSubContent>
-  </ContextMenuSub>
+  <ContextMenuItem inset v-if="panel?.isExpanded" @click="panel?.collapse"> Collapse sidebar </ContextMenuItem>
+  <ContextMenuItem inset v-else @click="panel?.expand">Expand sidebar</ContextMenuItem>
+  <ContextMenuSeparator />
+  <ContextMenuCheckboxItem
+    v-for="window in windowState"
+    :key="window.id"
+    :checked="window.opened && window.location == location"
+    :disabled="window.disabled"
+    @update:checked="
+      {
+        if (window.opened && window.location == location) {
+          window.opened = false;
+        } else {
+          window.opened = true;
+          window.location = location;
+        }
+      }
+    "
+  >
+    {{ window.title }}
+  </ContextMenuCheckboxItem>
 </template>
