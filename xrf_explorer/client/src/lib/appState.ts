146eb22c989ffbd497fca776fc62a3bde8dab42a
Jan Bulthuis <jbulthuis03@gmail.com>
2024-06-24 16:14:04 +0200

    Corrected for operator order of precedence

--- xrf_explorer/client/src/lib/appState.ts
+++ xrf_explorer/client/src/lib/appState.ts
@@ -39,8 +39,8 @@ export const datasource = computed(() => appState.workspace?.name ?? "");
 export const elements = computed(
   () => appState.workspace?.elementalChannels.filter((element) => element.enabled) ?? [],
 );
-export const elementalData = computed(() => appState.workspace?.elementalCubes.length ?? 0 > 0);
-export const spectralData = computed(() => appState.workspace?.spectralCubes.length ?? 0 > 0);
+export const elementalData = computed(() => (appState.workspace?.elementalCubes.length ?? 0) > 0);
+export const spectralData = computed(() => (appState.workspace?.spectralCubes.length ?? 0) > 0);
 
 /**
  * Type describing the state of the client.
