1b3f391daafc177d711121affec388fe13164070
IMaxiM03I <maxilea202@gmail.com>
2024-06-29 11:09:23 +0200

    fixed image-viewer/*

--- xrf_explorer/client/src/components/image-viewer/LayerSystem.vue
+++ xrf_explorer/client/src/components/image-viewer/LayerSystem.vue
@@ -106,10 +106,10 @@ function checkedOutsideLens(group: LayerGroup) {
                 v-for="property in properties.filter((prop) => !mainProperties.includes(prop.name))"
                 :key="property.name"
                 :label="property.name"
-                v-model="group[property.nameRef]"
                 :min="property.min"
                 :max="property.max"
                 :default="[property.default]"
+                v-model="group[property.nameRef]"
                 @update="() => setLayerGroupProperty(group, property.propertyName)"
               />
             </PopoverContent>
