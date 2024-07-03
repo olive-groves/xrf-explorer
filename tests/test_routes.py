24659841ae4a1276938fecf030c420212123d4cc
Dirk <d.burgers@student.tue.nl>
2024-07-03 17:26:49 +0200

    Modified test to use the app

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -4,24 +4,30 @@ from os.path import join
 import pytest
 import json
 
+from flask.testing import FlaskClient
+
+from xrf_explorer import app
 from xrf_explorer.server.file_system.helper import set_config
-from xrf_explorer.server.routes import (
-    list_accessible_data_sources
-)
 
 RESOURCES_PATH: str = join("tests", "resources")
 
 class TestRoutes:
     CUSTOM_CONFIG_PATH: str = join(RESOURCES_PATH, "configs", "routes.yml")
 
+    DATA_SOURCE: str = "test_data_source"
+
+    @pytest.fixture()
+    def client(self):
+        return app.test_client()
+
     @pytest.fixture(autouse=True)
     def setup_environment(self):
         set_config(self.CUSTOM_CONFIG_PATH)
         yield
 
-    def test_get_average_global(self):
+    def test_get_datasources(self, client: FlaskClient):
         # execute
-        result_str: str = list_accessible_data_sources()
+        result_str: str = client.get("/api/datasources").text
         result_list: list[str] = json.loads(result_str)
 
         # verify
