5be39ab4b0d3f61bdb42498ac70e19af32669f7b
nietFreek <128623776+nietFreek@users.noreply.github.com>
2025-10-07 15:00:38 +0200

    Sepps window is kut

---
+++ xrf_explorer/client/src/windows/StitchWindow.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+
+const emit = defineEmits(["close"]);
+
+function onPreview() {
+
+}
+
+function onExit() {
+  emit("close"); // close the window
+}
+
+</script>
+
+<template>
+  <Window title="Stitch" location="right">
+
+  </Window>
+</template>
