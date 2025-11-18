5b4dcf5f160ab1e5d095777e7ad35cc296a2cde2
PijkeB <p.j.v.bohemen@student.tue.nl>
2025-11-18 21:14:20 +0100

    ESLint fix

--- xrf_explorer/client/src/components/image-viewer/colorClusterHelper.ts
+++ xrf_explorer/client/src/components/image-viewer/colorClusterHelper.ts
@@ -50,9 +50,10 @@ function selectionUpdated(newSelection: ColorSegmentationSelection) {
       layerGroups.value.colorClusters.layers.forEach((layer) => {
         const filename =
           `${config.api.endpoint}/${datasource.value}/cs/bitmask/` +
-          `${newSelection.element}/${newSelection.k}/${newSelection.threshold}`
-            + (newSelection.useAreaSelection ? "/true" : "/false")
-            + "?v=" + newSelection.lastCompleteSelectionTimestamp;
+          `${newSelection.element}/${newSelection.k}/${newSelection.threshold}` +
+          (newSelection.useAreaSelection ? "/true" : "/false") +
+          "?v=" +
+          newSelection.lastCompleteSelectionTimestamp;
         // Only upload image if it changed
         if (layer.image !== filename) {
           disposeLayer(layer);
