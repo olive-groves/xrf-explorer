fc8bde10df255ba5dbd44ffebf35755ef60ff0cc
Meie Koning <132278034+MeieKoning@users.noreply.github.com>
2025-11-27 15:40:21 +0100

    Small fix to UI

--- xrf_explorer/client/src/App.vue
+++ xrf_explorer/client/src/App.vue
@@ -40,7 +40,7 @@ console.info("XRF-Explorer client created with configuration: ", props.config);
         <DRWindow />
         <LayerWindow />
         <WorkspaceWindow />
-        <FAQWindow v-if="faqWindowOpen" class="absolute left-0 top-0 z-[9999] size-full"></FAQWindow>
+        <FAQWindow v-if="faqWindowOpen" class="absolute top-0 left-0 w-full h-full z-[9999] bg-background" ></FAQWindow>
       </BaseContextMenu>
     </WindowContainer>
   </div>
