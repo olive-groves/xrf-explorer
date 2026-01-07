d2f26659c0d3eb2c7c13ad1f08f36bcf6a5b8091
DanielTheDev <danielkoopmans.nl@gmail.com>
2026-01-07 18:14:10 +0100

    Removed unused code

--- xrf_explorer/client/src/components/menus/Header.vue
+++ xrf_explorer/client/src/components/menus/Header.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 // Import the necessary components
-import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
+import { Menubar } from "@/components/ui/menubar";
 import { ExportMenu, FileMenu, MainMenu, WindowMenu } from ".";
-import { inject } from "vue";
-import { FrontendConfig } from "@/lib/config.ts";
-
-const config = inject<FrontendConfig>("config")!;
 
 /*const json1 = {
     type: "elemental",
@@ -84,82 +80,6 @@ const config = inject<FrontendConfig>("config")!;
     ],
   }; */
 
-const json2 = {
-  type: "spectral",
-  preview: false,
-  contextual_image: "RGB_resized.jpg",
-  down_scaling: 0.3,
-  fragments: [
-    {
-      datacube_file: "spectral1.raw",
-      rpl_file: "spectral1.rpl",
-      rotation: 270,
-      local_points: {
-        top_left: [40, 88],
-        top_right: [566, 25],
-        bottom_left: [92, 534],
-        bottom_right: [669, 544],
-      },
-      target_points: {
-        top_left: [31, 127],
-        top_right: [988, 5],
-        bottom_left: [124, 935],
-        bottom_right: [1167, 955],
-      },
-    },
-    {
-      datacube_file: "spectral2.raw",
-      rpl_file: "spectral2.rpl",
-      rotation: 270,
-      local_points: {
-        top_left: [19, 238],
-        top_right: [741, 270],
-        bottom_left: [63, 570],
-        bottom_right: [695, 560],
-      },
-      target_points: {
-        top_left: [15, 1485],
-        top_right: [1331, 1535],
-        bottom_left: [104, 2100],
-        bottom_right: [1251, 2071],
-      },
-    },
-    {
-      datacube_file: "spectral3.raw",
-      rpl_file: "spectral3.rpl",
-      rotation: 90,
-      local_points: {
-        top_left: [207, 48],
-        top_right: [666, 29],
-        bottom_left: [117, 499],
-        bottom_right: [762, 519],
-      },
-      target_points: {
-        top_left: [1841, 61],
-        top_right: [2678, 20],
-        bottom_left: [1667, 891],
-        bottom_right: [2864, 911],
-      },
-    },
-    {
-      datacube_file: "spectral4.raw",
-      rpl_file: "spectral4.rpl",
-      rotation: 90,
-      local_points: {
-        top_left: [94, 232],
-        top_right: [661, 155],
-        bottom_left: [90, 639],
-        bottom_right: [793, 761],
-      },
-      target_points: {
-        top_left: [1661, 1428],
-        top_right: [2702, 1262],
-        bottom_left: [1679, 2161],
-        bottom_right: [2946, 2370],
-      },
-    },
-  ],
-};
 
 /*const json3 = {
   type: "spectral",
@@ -183,81 +103,6 @@ const json2 = {
     },
   ],
 }; */
-
-const json4 = {
-  type: "spectral",
-  contextual_image: "RGB_resized.jpg",
-  fragments: [
-    {
-      datacube_file: "spectral1.raw",
-      rpl_file: "spectral1.rpl",
-      rotation: 270,
-      local_points: {
-        top_left: [40, 88],
-        top_right: [566, 25],
-        bottom_left: [92, 534],
-        bottom_right: [669, 544],
-      },
-      target_points: {
-        top_left: [31, 127],
-        top_right: [988, 5],
-        bottom_left: [124, 935],
-        bottom_right: [1167, 955],
-      },
-    },
-    {
-      datacube_file: "spectral2.raw",
-      rpl_file: "spectral2.rpl",
-      rotation: 270,
-      local_points: {
-        top_left: [19, 238],
-        top_right: [741, 270],
-        bottom_left: [63, 570],
-        bottom_right: [695, 560],
-      },
-      target_points: {
-        top_left: [15, 1485],
-        top_right: [1331, 1535],
-        bottom_left: [104, 2100],
-        bottom_right: [1251, 2071],
-      },
-    },
-    {
-      datacube_file: "spectral3.raw",
-      rpl_file: "spectral3.rpl",
-      rotation: 90,
-      local_points: {
-        top_left: [207, 48],
-        top_right: [666, 29],
-        bottom_left: [117, 499],
-        bottom_right: [762, 519],
-      },
-      target_points: {
-        top_left: [1841, 61],
-        top_right: [2678, 20],
-        bottom_left: [1667, 891],
-        bottom_right: [2864, 911],
-      },
-    },
-    {
-      datacube_file: "spectral4.raw",
-      rpl_file: "spectral4.rpl",
-      rotation: 90,
-      local_points: {
-        top_left: [94, 232],
-        top_right: [661, 155],
-        bottom_left: [90, 639],
-        bottom_right: [793, 761],
-      },
-      target_points: {
-        top_left: [1661, 1428.44],
-        top_right: [2702, 1262],
-        bottom_left: [1679, 2161],
-        bottom_right: [2946, 2370],
-      },
-    },
-  ],
-};
 </script>
 
 <template>
