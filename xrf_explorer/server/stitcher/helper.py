Fix rotation problem with the API call for stitching

xrf_explorer/server/stitcher/helper.py
@@ -33,10 +33,10 @@ class WarpSelection:
                 of the bottom-right corner of the rectangle.
             total_height: The total height of the related image. Used for converting cartesian coordinated to graphic/screen coordinates.
         """
-        self.top_left = (top_left[0], total_height - top_left[1])
-        self.top_right = (top_right[0], total_height - top_right[1])
-        self.bottom_left = (bottom_left[0], total_height - bottom_left[1])
-        self.bottom_right = (bottom_right[0], total_height - bottom_right[1])
+        self.top_left = (top_left[0], total_height)
+        self.top_right = (top_right[0], total_height)
+        self.bottom_left = (bottom_left[0], total_height)
+        self.bottom_right = (bottom_right[0], total_height)
 
     def get_points(self, scalar: float = 1.0) -> np.ndarray:
         """
@@ -71,8 +71,13 @@ class WarpSelection:
 
     def are_within(self, width, height) -> bool:
         """Checks if all points are within the given width and height."""
-        for point in [self.top_left, self.top_right, self.bottom_left, self.bottom_right]:
-            if not (0 <= point[0] <= width and 0 <= point[1] <= height):
+        for x, y in (
+            self.top_left,
+            self.top_right,
+            self.bottom_left,
+            self.bottom_right,
+        ):
+            if not (0 <= x < width and 0 <= y < height):
                 return False
         return True
 