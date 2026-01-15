a46a768987fb75f4ef8405b6794816a33b3d643c
nietFreek <128623776+nietFreek@users.noreply.github.com>
2026-01-15 20:39:35 +0100

    fix?

--- xrf_explorer/client/src/components/image-viewer/stitchHelper.ts
+++ xrf_explorer/client/src/components/image-viewer/stitchHelper.ts
@@ -195,26 +195,17 @@ export async function confirmStitching(includeSpectral: boolean, includeElementa
     ws.elementalCubes = []
     saveWorkspaceDebounced();
     await stitch(false, "elemental", scaling_factor, intensities);
+    requestWorkspace();
   }
   if (includeSpectral) {
     ws.spectralCubes = []
     saveWorkspaceDebounced();
     await stitch(false, "spectral", scaling_factor, intensities);
+    requestWorkspace();
   }
 
-  let workspacePollVersion = 0;
-
   const interval = setInterval(async () => {
-    workspacePollVersion++;
-
-    const resp = await fetch(
-      `/api/${ws.name}/workspace?version=${workspacePollVersion}`,
-      { cache: "no-store" }
-    );
-    if (!resp.ok) return;
-
-    const updated = await resp.json();
-
+    const updated = await requestWorkspace();
     const spectralDone =
       !includeSpectral || (updated.spectralCubes?.length ?? 0) > 0;
     const elementalDone =
@@ -223,7 +214,6 @@ export async function confirmStitching(includeSpectral: boolean, includeElementa
     if (spectralDone && elementalDone) {
       clearInterval(interval);
 
-      appState.workspace = updated;
       const ws = appState.workspace
       if (!ws) {return}
       ws.stitchingMode = "full"
@@ -233,6 +223,24 @@ export async function confirmStitching(includeSpectral: boolean, includeElementa
       saveWorkspaceDebounced();
     }
   }, 2000);
+}
+
+let workspacePollVersion = 0;
+
+async function requestWorkspace() {
+  const ws = appState.workspace;
+  if (!ws) return;
+  workspacePollVersion++;
+
+  const resp = await fetch(
+    `/api/${ws.name}/workspace?version=${workspacePollVersion}`,
+    { cache: "no-store" }
+  );
+  if (!resp.ok) return;
+
+  const updated = await resp.json();
+  appState.workspace = updated;
+  return updated
 }
 
 
