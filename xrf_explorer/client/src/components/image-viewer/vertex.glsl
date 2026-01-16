1417b7f7443644735ef3b58c24dbb176d5e8f9a5
PijkeB <p.j.v.bohemen@student.tue.nl>
2026-01-16 03:02:19 +0100

    Reverted shader changes from a while back
    In an attempt to get rotation for greyscales in the stitchviewer working the recipe mapping broke.
    That rotation is now handled differently so we can just discard the changes made to the vertex shader.

--- xrf_explorer/client/src/components/image-viewer/vertex.glsl
+++ xrf_explorer/client/src/components/image-viewer/vertex.glsl
@@ -2,9 +2,9 @@ precision highp float;
 precision highp int;
 
 uniform float iIndex;
-uniform vec4 iViewport;
-uniform mat3 mRegister;
+uniform vec4 iViewport; // x, y, w, h
 
+// Attributes
 in vec3 position;
 in vec2 uv;
 
@@ -13,12 +13,11 @@ out vec2 vUv;
 void main() {
   vUv = uv;
 
-  // Rotate/transform in image space
-  vec3 p = mRegister * vec3(position.xy, 1.0);
-
-  // Convert to clip space using viewport
-  float px = 2.0 * (p.x - iViewport.x) / iViewport.z - 1.0;
-  float py = 2.0 * (p.y - iViewport.y) / iViewport.w - 1.0;
-
-  gl_Position = vec4(px, py, iIndex / 1024.0, 1.0);
+  // Transform to viewport
+  gl_Position = vec4(
+    2.0 * (position.x - iViewport.x) / iViewport.z - 1.0,
+    2.0 * (position.y - iViewport.y) / iViewport.w - 1.0,
+    iIndex / 1024.0,
+    1.0
+  );
 }
