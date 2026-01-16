19fe3bb16aa49d2490322765136e24e869bdedef
PijkeB <p.j.v.bohemen@student.tue.nl>
2026-01-16 05:28:46 +0100

    Reintroduce cache for contextual images

--- xrf_explorer/server/routes/images.py
+++ xrf_explorer/server/routes/images.py
@@ -52,9 +52,7 @@ def contextual_image(data_source: str, name: str):
 
     # Ensure that the converted images are cached by the client
     response = send_file(image_io, mimetype='image/png')
-    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "0"
+    response.headers["Cache-Control"] = "public, max-age=604800, immutable"
     return response
 
 
